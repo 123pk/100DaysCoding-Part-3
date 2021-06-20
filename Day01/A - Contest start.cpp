71aebb855600371035a5638d2bba1e6c410f8d27
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-20 18:33:51 +0530

    Update A - Contest start.cpp

--- Day01/A - Contest start.cpp
+++ Day01/A - Contest start.cpp
@@ -4,3 +4,27 @@ Contest :- Codeforces Round 727
 Problem :- A - Contest 
 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long a,b,c;
+        cin>>a>>b>>c;
+        long long temp=c/b;
+        
+        long long ans=0;
+        if(a<temp){
+             
+            ans+=(a*(a-1))/2;
+        }
+        else{
+            ans+=(a-temp)*temp;
+            ans+=(temp*(temp-1))/2;
+        }
+        cout<<ans<<endl;
+    }
+}
