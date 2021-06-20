a55a949186b597c96fda844005ea1e5907eee885
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-20 18:43:23 +0530

    Update B - love song.cpp

--- Day01/B - love song.cpp
+++ Day01/B - love song.cpp
@@ -6,3 +6,48 @@ Approach :- We can store occurence of each word of alphabet at each index of str
             now for each [l,r] we will find two array one storing occurence of each alphabet at index 'l' and other at 'r', we will find [ Ar[i] - Al[i]] which will give us 
             occurenece of each alphabet in range [l,r], now we will run a loop from 'a' to 'z' and update our answer by [ count*(index_of_corresponding_letter_in_alphabet)]
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n,q;
+   cin>>n>>q;
+   
+   string s;
+   cin>>s;
+   
+   vector<vector<int>>P;
+   vector<int>temp(26);
+   for(int i=0;i<s.size();++i){
+       temp[s[i]-'a']++;
+       P.push_back(temp);
+   }
+   
+   map<char,int>Q;
+   for(char ch='a';ch<='z';++ch){
+       Q[ch]=(ch-'a')+1;
+   }
+   
+   for(int i=0;i<q;++i){
+       int l,r;
+       cin>>l>>r;
+       l--;
+       r--;
+       vector<int>x=P[r];
+      if(l){
+          vector<int>y=P[l-1];
+          for(int j=0;j<26;++j){
+              x[j]-=y[j];
+          }
+      }
+      
+      long long ans=0;
+      for(int j=0;j<26;++j){
+          if(x[j]){
+              ans+=(x[j]*(j+1));
+          }
+      }
+      cout<<ans<<"\n";
+   }
+}
