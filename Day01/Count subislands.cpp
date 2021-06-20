e796a9ee96a0502b73ea5812752fb2d1af5799f4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-20 11:15:23 +0530

    Create Count subislands.cpp

---
+++ Day01/Count subislands.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Problem :- Count sub islands
+Contest :- Weekly contest 246
+Approach :-  we do bfs and visit all the nodes in all 4 directions if value of grid 2 at current index is '1' and is not visited , if all the nodes that we visited are island in
+            grid 1 also then we incrment our count by one
+*/
