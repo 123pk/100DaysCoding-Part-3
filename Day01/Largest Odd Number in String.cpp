dd83049442f811d2c42b9a60644937217a446cc9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-20 10:51:05 +0530

    Create Largest Odd Number in String.cpp

---
+++ Day01/Largest Odd Number in String.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Largest Odd Number in String
+Contest :- Weekly contest 246
+Approach :- find the last index where we have odd number , the largest odd number is substring from start to the last odd number index
+*/
