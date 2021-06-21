9ed618b6d324fec9306369ce54aa876d91a2c6be
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-21 22:15:48 +0530

    Update Stable Groups.cpp

--- Day02/Stable Groups.cpp
+++ Day02/Stable Groups.cpp
@@ -2,4 +2,5 @@
 Platform :- Codeforces 
 Contest :- Codeforces Round 727 Div 2
 Poblem :- C - Stable Groups
+Approach :- It was a greedy problem where we need to greedily choose where to put random students
 */
