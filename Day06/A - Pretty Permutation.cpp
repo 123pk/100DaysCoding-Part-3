d4bcddf74927754116e9bd9746ef96d519e6e47a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-25 23:30:30 +0530

    Create A - Pretty Permutation.cpp

---
+++ Day06/A - Pretty Permutation.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 728 Div 2
+Problem :- A - Pretty Permutations 
+Approach :- if n is even we can do adjacenet swap to every 2nd element to for [2,1,4,3] from [1,2,3,4] in case of odd value of n we need an extra swap 
+*/
