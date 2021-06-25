a3e46f48ce7ea5c25a15b51fb3a8abad206f0399
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-25 23:30:54 +0530

    Update A - Pretty Permutation.cpp

--- Day06/A - Pretty Permutation.cpp
+++ Day06/A - Pretty Permutation.cpp
@@ -4,3 +4,29 @@ Contest :- Codeforces Round 728 Div 2
 Problem :- A - Pretty Permutations 
 Approach :- if n is even we can do adjacenet swap to every 2nd element to for [2,1,4,3] from [1,2,3,4] in case of odd value of n we need an extra swap 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        if(n%2){
+            cout<<3<<" "<<1<<" "<<2<<" ";
+            for(int i=5;i<=n;i+=2){
+                cout<<i<<" "<<i-1<<" ";
+            }
+            cout<<endl;
+        }
+        else{
+            for(int i=2;i<=n;i+=2){
+                cout<<i<<" "<<i-1<<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
