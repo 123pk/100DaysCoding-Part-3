69ac5ce12915060556b5df88daf53e8673cbb7b6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-25 23:40:39 +0530

    Create B - Pleasant Pairs.cpp

---
+++ Day06/B - Pleasant Pairs.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 728 Div 2
+Approach :- for a given n we can have value of (i+j) from (3 to 2*n-1), we will find the factors of each i from 3 to (2*n-1) and if we have two distinct factors such
+            that sum of their indexes is equal to i then we increment our count.
+*/
