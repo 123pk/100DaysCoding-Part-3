98e9a45dff15b4bc4d952374b0311a614fa511e6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-26 22:48:26 +0530

    Create Unpleasant One.cpp

---
+++ Day07/Unpleasant One.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- June Lunctime 2021
+Problem :- Unpleasant One (UNONE)
+Approach :- It is productive to have all even numbers together and before odd numbers ,so print all the even numbers first then odd numbers
+*/
