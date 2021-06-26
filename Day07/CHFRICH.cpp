4e30a359fb48c763832744e678e5817baac4c1db
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-26 23:01:45 +0530

    Update CHFRICH.cpp

--- Day07/CHFRICH.cpp
+++ Day07/CHFRICH.cpp
@@ -5,3 +5,22 @@ Contest :- Codechef June Lunctime 2021
 Problem :- CHFRICH
 Approach :- ceil ([Final amount - Intitial amount ]/ x)
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    long long a,b,x;
+	    cin>>a>>b>>x;
+	    
+	    long long temp=b-a;
+	    long long ans=temp/x;
+	    
+	    if(temp%x)ans+=1;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
