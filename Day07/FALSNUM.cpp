e54f1be2e0a7d045c473fba9590bfe2e1012dfcc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-26 22:57:03 +0530

    Create FALSNUM.cpp

---
+++ Day07/FALSNUM.cpp
@@ -0,0 +1,10 @@
+/*
+Platform :- Codechef
+Contest:- June Lunch time 2021
+Problem :- False Number (FALSENUM)
+Approach :- we can divide into two case 
+           case 1:- starting character is '1'
+                   in this case we traverse from 1 to n , n = length of string and as soon as we found a character that is not '0' we add '0'
+           case 2:- starting character is not '1'
+                   here we add '1' before current string
+*/
