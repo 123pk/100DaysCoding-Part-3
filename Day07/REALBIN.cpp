9393e2c15a248b1d17e94abbbfe102b9f763ba6a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-26 22:52:04 +0530

    Update REALBIN.cpp

--- Day07/REALBIN.cpp
+++ Day07/REALBIN.cpp
@@ -3,4 +3,5 @@ Platform :- Codechef
 Contest :- June Lunchtime
 Problem :- From Rational to Binary (REALBIN)
 Approach :- we can only do when the denominator is power of 2
+Alert :- To avoid TLE use '\n' instead of 'endl' in c++
 */
