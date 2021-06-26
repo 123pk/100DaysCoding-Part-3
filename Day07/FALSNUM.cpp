3930aba962396978aabd4c07f91737915e9c9d7e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-26 22:59:14 +0530

    Update FALSNUM.cpp

--- Day07/FALSNUM.cpp
+++ Day07/FALSNUM.cpp
@@ -8,3 +8,39 @@ Approach :- we can divide into two case
            case 2:- starting character is not '1'
                    here we add '1' before current string
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int t;
+     cin>>t;
+     
+     while(t--){
+         string s;
+         cin>>s;
+         
+         string ans="";
+         if(s[0]=='1'){
+             int f=0;
+             ans+=s[0];
+             for(int i=1;i<s.size();++i){
+                 if(f)ans+=s[i];
+                 else{
+                 if(s[i]!='0'){
+                     f=1;
+                     ans+='0';
+                     ans+=s[i];
+                 }
+                 }
+             }
+             
+             if(f==0)ans+='0';
+         }
+         else{
+             ans+='1';
+             ans+=s;
+         }
+         cout<<ans<<endl;
+     }
+}
