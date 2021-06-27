e4962f08e3540c321f999962ef27e8ef13a8379e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-27 23:17:13 +0530

    Create Kefa and Park.cpp

---
+++ Day08/Kefa and Park.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 321 Div 2
+Approach :- We will do BFS on tree and if at any point we have more than 'm' nodes we will not visit it childs and if we managed to get to leaf we will increment our answer by 1
+*/
