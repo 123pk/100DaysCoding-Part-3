30093080a3e2f3c1ee77e3331cd1b85bd8dba97c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-28 21:28:46 +0530

    Update Remove All Adjacent Duplicates In String.cpp

--- Day09/Remove All Adjacent Duplicates In String.cpp
+++ Day09/Remove All Adjacent Duplicates In String.cpp
@@ -4,3 +4,27 @@ Problem :- Remove All Adjacent Duplicates In String
 Approach :- we will use stack and push element in stack and if top elememnt of stack is equal to current element we pop it out of stack , at the end the string 
             formed by left characters in stack in correct order is our answer 
 */
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans;
+        stack<char>P;
+        for(auto x:s){
+            if(!P.empty()){
+                if(P.top()==x){
+                    P.pop();
+                }
+                else P.push(x);
+            }
+            else P.push(x);
+        }
+        
+        while(!P.empty()){
+            ans+=P.top();
+            P.pop();
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
