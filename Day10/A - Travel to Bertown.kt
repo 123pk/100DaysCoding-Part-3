62dc626d3d089b06ce086a35e223514aadfe02e2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-29 22:42:55 +0530

    Create A - Travel to Bertown.kt

---
+++ Day10/A - Travel to Bertown.kt
@@ -0,0 +1,6 @@
+/**
+Platform :- Codeforces
+Contest :- Kotlin Heros :- Episode 7
+Hint :- find the biggest value [ if ( s[i]<= 'b' ) where s[i] is the start time for every friend and 'b' is time at which she arrives in Bertown ],
+         of (e[i]-'b'] where e[i] is end time
+*/
