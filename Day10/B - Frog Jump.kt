d03a1b71196b05517ba3fc82f865effb87d96e64
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-29 22:38:59 +0530

    Update B - Frog Jump.kt

--- Day10/B - Frog Jump.kt
+++ Day10/B - Frog Jump.kt
@@ -4,3 +4,21 @@ Contest :- Kotlin Practise Episode 7
 Approach :- simple constructive expression , if 'n' is odd we have one move with 'a' steps and (n-1)/2 move with (a-b) step
             else if 'n' is even we have (n/2) move with (a-b) steps
 */
+fun main(args: Array<String>) {
+    val t = Integer.valueOf(readLine())
+    for(i in 1..t)
+    {
+      val (a,b,c)=readLine()!!.split(' ').map(String::toLong)
+      var ans:Long = 1 
+      if(c%2==ans){
+         var d=(c-1)/2
+         ans = d*(a-b) +a
+      }
+      else{
+         var d=c/2
+         ans= d*(a-b)
+      }
+      println("$ans")
+      
+    }
+}
