95d2f095e5231ccc2b0168f6dde1710c054f3674
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-29 22:35:54 +0530

    Create B - Frog Jump.kt

---
+++ B - Frog Jump.kt
@@ -0,0 +1,6 @@
+/**
+Platform :- Codeforces
+Contest :- Kotlin Practise Episode 7 
+Approach :- simple constructive expression , if 'n' is odd we have one move with 'a' steps and (n-1)/2 move with (a-b) step
+            else if 'n' is even we have (n/2) move with (a-b) steps
+*/
