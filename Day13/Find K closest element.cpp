6e31712bdfff0bb50c4de5b6c6c31c325e8178f2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-02 20:39:31 +0530

    Update Find K closest element.cpp

--- Day13/Find K closest element.cpp
+++ Day13/Find K closest element.cpp
@@ -4,3 +4,19 @@ Problem :- Find K closest element
 Approach :- for given value of x find the relative distance from each A[i] and sort according to relative distance/absolute difference
             add first 'k' elemnts to answer array and sort it in asceding order and return ans
 */
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int y) {
+        vector<int>ans;
+        vector<pair<int,int>>P;
+        for(auto &x:arr){
+            P.push_back({abs(x-y),x});
+        }
+        
+        sort(P.begin(),P.end());
+        
+        for(int i=0;i<k;++i)ans.push_back(P[i].second);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
