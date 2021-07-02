060e68d5b96c70f534a1a22d41e16941e748b1a7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-02 20:39:16 +0530

    Create Find K closest element.cpp

---
+++ Day13/Find K closest element.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Find K closest element 
+Approach :- for given value of x find the relative distance from each A[i] and sort according to relative distance/absolute difference
+            add first 'k' elemnts to answer array and sort it in asceding order and return ans
+*/
