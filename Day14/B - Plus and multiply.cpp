debaaf4a57fae88e21423f302f0309a25fa0d984
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-03 21:37:47 +0530

    Create B - Plus and multiply.cpp

---
+++ Day14/B - Plus and multiply.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 729 Div 2
+Approach :- let rem=1 we will increment rem by 'a' and will check if (n-rem)%b==0 till the time rem<=n if so we print "Yes" else "No"
+*/
