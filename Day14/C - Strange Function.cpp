17e325f84ac8b7c3360930b59bfeeb37d9e1a9d9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-03 21:28:57 +0530

    Create C - Strange Function.cpp

---
+++ Day14/C - Strange Function.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 729 Div 2
+Approach :- we find the product of values from 1.. to i by and also removing GCD values we do so by the time this values is less than 'n' and add ans+(n/this_value) 
+            take care of mod also
+*/
