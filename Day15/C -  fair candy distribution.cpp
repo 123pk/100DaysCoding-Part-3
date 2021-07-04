1d98d5a66f520784e7115cc3f8ed0a6086bff8e1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-04 21:58:13 +0530

    Update C -  fair candy distribution.cpp

--- Day15/C -  fair candy distribution.cpp
+++ Day15/C -  fair candy distribution.cpp
@@ -3,3 +3,39 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner contest 208
 Approach :- if 'k' is greater than 'n' then each one will get (k/n) candies , the remaining k%n candies will be distributed to first smallest (k%n) values
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comp(pair<long long,int>&P,pair<long long,int>&Q){
+    return P.second<Q.second;
+}
+
+int main(){
+
+     long long k;
+     int n;
+     cin>>n>>k;
+     
+     long long A[n];
+     vector<pair<long long,int>>P;
+     for(int i=0;i<n;++i){
+         cin>>A[i];
+         P.push_back({A[i],i});
+     }
+     
+     sort(P.begin(),P.end());
+     long long z1=k/n;
+     long long x=k%n;
+     for(int i=0;i<n;++i){
+         if(i<x)P[i].first=z1+1;
+         else P[i].first=z1;
+     }
+     
+     sort(P.begin(),P.end(),comp);
+     
+     for(auto &x:P){
+         cout<<x.first<<" \n";
+     }
+ 
+     
+}
