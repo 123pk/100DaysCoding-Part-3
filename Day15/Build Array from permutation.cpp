095a4f43e379c0a76a0a745a7e6e897187378c3d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-04 21:43:37 +0530

    Update Build Array from permutation.cpp

--- Day15/Build Array from permutation.cpp
+++ Day15/Build Array from permutation.cpp
@@ -3,3 +3,15 @@ Platform :- Leetcode
 Problem :- Build Array from permutation
 Approach :- make an array such that arr[i] = nums[nums[i]] 
 */
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();++i){
+            ans.push_back(nums[nums[i]]);
+        }
+        
+        return  ans;
+    }
+};
