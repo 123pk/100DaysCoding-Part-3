ba680762f8fff71564c8b21b22fdec90e14d5959
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-04 21:52:16 +0530

    Create B - factorial yen coin.cpp

---
+++ Day15/B - factorial yen coin.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Problem :- B - factorial yen coin
+Approach :- store all the value from 1! to 10! and for a given value find the maximum value less than equal to 'x' that can be acheived and substract it from 'x' and 
+            accordingly update the count
+*/
