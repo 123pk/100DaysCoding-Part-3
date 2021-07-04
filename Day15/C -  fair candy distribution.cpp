dfc107d4662970542f3503a54bc7d69667c79840
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-04 21:57:55 +0530

    Create C -  fair candy distribution.cpp

---
+++ Day15/C -  fair candy distribution.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner contest 208
+Approach :- if 'k' is greater than 'n' then each one will get (k/n) candies , the remaining k%n candies will be distributed to first smallest (k%n) values
+*/
