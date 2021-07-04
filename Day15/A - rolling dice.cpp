7c8bf6a210600d3a21d769600d8964f82152c142
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-04 21:59:06 +0530

    Update A - rolling dice.cpp

--- Day15/A - rolling dice.cpp
+++ Day15/A - rolling dice.cpp
@@ -3,3 +3,16 @@ Platform :- Atcoder
 Problem :- A - Rolling dice
 Approach :- For a given 'n' if we have value between n and n*6 inclusive then we can get that value else not 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n,k;
+    cin>>n>>k;
+    
+    long long low=n;
+    long long h=n*6;
+    
+    if(k<=h && low<=k)cout<<"Yes\n";
+    else cout<<"No\n";
+}
