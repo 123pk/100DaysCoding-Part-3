54ce4f29a1b16dbd9b5cf9fb6318cb8b2c8d82f1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-04 21:59:26 +0530

    Update B - factorial yen coin.cpp

--- Day15/B - factorial yen coin.cpp
+++ Day15/B - factorial yen coin.cpp
@@ -4,3 +4,31 @@ Problem :- B - factorial yen coin
 Approach :- store all the value from 1! to 10! and for a given value find the maximum value less than equal to 'x' that can be acheived and substract it from 'x' and 
             accordingly update the count
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    long long x;
+    cin>>x;
+    
+    long long val=1;
+    vector<long long>P;
+    for(int i=1;i<=10;++i){
+        val*=i;
+        P.push_back(val);
+    }
+    
+    int count=0;
+ 
+        for(int i=9;i>=0;--i){
+            if(P[i]<=x){
+                int temp=(x/P[i]);
+                long long zz=temp*P[i];
+                x-=zz;
+                count+=temp;
+            }
+        }
+        cout<<count<<"\n";
+ 
+}
