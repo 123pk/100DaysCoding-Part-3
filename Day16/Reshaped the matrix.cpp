d85ce11198b6b8f5860aa3cdde52efff24dc3460
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-05 21:13:38 +0530

    Create Reshaped the matrix.cpp

---
+++ Day16/Reshaped the matrix.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Reshaped the matrix
+Approach :- if n*m!= r*c return the same matrix where n=row of matrix given , m = coloumn of given matrix , 
+            else traverse the matrix and store in array create a new of array of new dimensions and store the element of array
+*/
