d2913e21a36f2bfe45e1c311ae57239aa03065a9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-06 22:57:19 +0530

    Create Reduce Array size to half.cpp

---
+++ Day17/Reduce Array size to half.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- sort the array based on their  frequency in decresing order , remove element with highest frequency out of all continue this until you acheive size <= half
+*/
