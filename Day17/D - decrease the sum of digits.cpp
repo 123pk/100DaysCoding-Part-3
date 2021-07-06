892f124480097666bce96f3d65791800299f6e66
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-06 23:03:07 +0530

    Create D - decrease the sum of digits.cpp

---
+++ Day17/D - decrease the sum of digits.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 667 Div 3
+Approach :- convert number into string reverse the string and traverse till the time sum of digits >k , make them to zero and take care of carry which will be taken forward 
+*/
