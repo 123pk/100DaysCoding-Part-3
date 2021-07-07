7aeff24c1387dcbc6b60bd7a9bef184c947fe277
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-07 22:05:17 +0530

    Update House Robber III.cpp

--- Day18/House Robber III.cpp
+++ Day18/House Robber III.cpp
@@ -2,3 +2,19 @@
 Platform :- Leetcode
 Hint :- dfs on trees 
 */
+class Solution {
+    pair<int,int> dfs(TreeNode* root){
+        if(!root){
+            return {0,0};
+        }
+        pair<int,int>left = dfs(root->left);
+        pair<int,int>right = dfs(root->right);
+        return make_pair( root->val+left.second+right.second ,
+                         (max(left.first,left.second)+max(right.first,right.second)));
+    }
+public:
+    int rob(TreeNode* root) {
+        pair<int,int>ans = dfs(root);
+        return max(ans.first,ans.second);
+    }
+};
