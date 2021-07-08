ab6ee88ace790b5709037ca63b0c2d4a87b64c56
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-08 21:47:57 +0530

    Update RPD and RAP sheet.cpp

--- Day19/RPD and RAP sheet.cpp
+++ Day19/RPD and RAP sheet.cpp
@@ -3,3 +3,25 @@ Platform :- Codeforces
 Contest :- Codeforces Round 730 #Div 2
 Problem :- RPD and RAP sheet
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        
+        int c;
+        int start=0;
+        
+        for(int i=0;i<n;++i){
+            cout<<(i ^ start)<<endl;
+            cin>>c;
+            cout.flush();
+            if(c)break;
+            start^=(i^start);
+        }
+    }
+}
