1464e1614c8e7eefa392548dd8b2fb6675fdb6c6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-08 22:33:48 +0530

    Update and rename minimize size subarray sum.cpp to minimum size subarray sum.cpp

--- Day19/minimum size subarray sum.cpp
+++ Day19/minimum size subarray sum.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Leetcode
-Problem :- minimize size subarray sum
+Problem :- minimum size subarray sum
 Approach :- we traverse the array ans take two pointer both is initialised to (start = 0 & end = 0) in beginning as we move ahead we do ( tot + = nums[i] and end =i )
             if the value is ( tot > target && (tot - nums[start] >target)) we do ( tot - = nums[start] ,start++) till the time ( start < end ) and our condition is satisfied   
             at the end if our ( tot>= target) we store the min value of ( (end -start)+1) in ans
