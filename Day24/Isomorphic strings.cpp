49023ff075c512db62d5ed7e0956f80d03d33332
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-13 12:48:46 +0530

    Create Isomorphic strings.cpp

---
+++ Day24/Isomorphic strings.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- use vector of int to store indices of characters for both the strings , then run a loop from 0 to len-1 and if the vector of ith character is 
+            same then continue else return false .
+*/
