dffde3a7ca6cfd8380564067a3acdf1e9ce3c019
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-13 12:24:30 +0530

    Update Find median for a data stream.cpp

--- Day23/Find median for a data stream.cpp
+++ Day23/Find median for a data stream.cpp
@@ -2,3 +2,24 @@
 Platform :- Leetcode
 Problem :- Find median for a data stream
 */
+class MedianFinder {
+//public:
+   priority_queue<long> small, large;
+public:
+
+    void addNum(int num) {
+        small.push(num);
+        large.push(-small.top());
+        small.pop();
+        if (small.size() < large.size()) {
+            small.push(-large.top());
+            large.pop();
+        }
+    }
+
+    double findMedian() {
+        return small.size() > large.size()
+               ? small.top()
+               : (small.top() - large.top()) / 2.0;
+    }
+};
