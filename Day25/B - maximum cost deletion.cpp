235bcf333d1ca5a70c0271a5e72dbdf80fa8c451
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-14 23:32:23 +0530

    Update B - maximum cost deletion.cpp

--- Day25/B - maximum cost deletion.cpp
+++ Day25/B - maximum cost deletion.cpp
@@ -3,3 +3,21 @@ Platform :- Codeforces
 Contest :- Educational Round 111
 Approach :- greedily choose substring to be deleted which will increase total cost
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int ans=sqrt(n);
+        int z=ans;
+        z*=z;
+        if(z!=n)ans++;
+        cout<<ans<<"\n";
+    }
+}
