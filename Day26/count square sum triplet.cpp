6db1eb77d95468733c7698c2dbdbc6db4ced5c9d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-15 20:37:24 +0530

    Update count square sum triplet.cpp

--- Day26/count square sum triplet.cpp
+++ Day26/count square sum triplet.cpp
@@ -3,3 +3,19 @@ Platform :- Leetcode
 Problem :- count square sum triplet
 Approach :- brute force three loops for a,b,c if condition is satisfied increment count 
 */
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans=0;
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=n;++j){
+                for(int c=1;c<=n;++c){
+                    int tot=i*i +(j*j);
+                    int temp=c*c;
+                    if(tot==temp)ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
