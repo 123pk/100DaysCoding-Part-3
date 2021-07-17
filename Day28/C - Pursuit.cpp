bc5ae3f7161e717afa39ca6c2be4dd3e9cb7edb2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-17 23:16:07 +0530

    Create C - Pursuit.cpp

---
+++ Day28/C - Pursuit.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round #733 (Div. 1 + Div. 2, based on VK Cup 2021 - Elimination (Engine))
+Approach :- to increase you value you add 100 to your performance and 0 to Illya's .we do so till the time score of (sz-(sz/4)) values of yours < illya's
+*/
