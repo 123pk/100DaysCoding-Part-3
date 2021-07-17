f7ce4352ff615f21efe934d0f70a2797582ec9ff
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-17 23:52:41 +0530

    Update A - Binary Decimal.cpp

--- Day28/A - Binary Decimal.cpp
+++ Day28/A - Binary Decimal.cpp
@@ -3,3 +3,22 @@ Platform :- Codeforces
 Contest :- Codeforces Round #733 (Div. 1 + Div. 2, based on VK Cup 2021 - Elimination (Engine))
 Approach :- the max value of digit in 'n'
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        
+        int ans=0;
+        for(auto &x:s){
+            ans=max(ans,x-'0');
+        }
+        cout<<ans<<"\n";
+    }
+    
+}
