e466ab278c8ae13dbce8fa63b1aeb58a0d74f432
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-18 18:29:27 +0530

    Update Maximum number of words you can type.cpp

--- Day29/Maximum number of words you can type.cpp
+++ Day29/Maximum number of words you can type.cpp
@@ -4,3 +4,38 @@ Problem :- Maximum number of words you can type
 Contest :- Leetcode Weekly contest 250
 Approach :- If the words don't contain characters of brokenletters then we can type that word
 */
+class Solution {
+public:
+    int canBeTypedWords(string text, string A) {
+        text+=' ';
+        vector<string>P;
+        string temp;
+        for(auto &x:text){
+            if(x==' '){
+                
+                P.push_back(temp);temp="";
+            }
+            else temp+=x;
+        }
+        
+        
+        int ans=0;
+        vector<int>Q(26);
+        for(auto &x:A){
+            Q[x-'a']++;
+        }
+        
+        for(auto &x:P){
+            int f=0;
+            for(auto &y:x){
+                if(Q[y-'a']){
+                    f=1;break;
+                }
+            }
+            
+            if(f==0)ans++;
+        }
+        return ans;
+        
+    }
+};
