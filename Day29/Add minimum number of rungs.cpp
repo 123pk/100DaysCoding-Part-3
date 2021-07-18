0b5fcc9d4a79dac46533b5f952334229cc740b98
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-18 18:32:42 +0530

    Update Add minimum number of rungs.cpp

--- Day29/Add minimum number of rungs.cpp
+++ Day29/Add minimum number of rungs.cpp
@@ -3,3 +3,26 @@ Platform :- Leetcode
 Contest :- Leetcode weekly contest 250
 Approach :- if the difference between two adjacent values of array is more than 'dist' then we need to add ((A[i]-A[i-1])-1)/dist  rungs to it
 */
+class Solution {
+public:
+    int addRungs(vector<int>& rugs, int d) {
+        int ans=0;
+        int temp=0;
+        for(int i=0;i<rugs.size();++i){
+            int z=(rugs[i]-temp);
+            if(z>d){
+                
+                int val=rugs[i]-(temp+1);
+     //           cout<<val<<" "<<temp<<" ";
+                ans+=(val/d);
+                
+                cout<<temp<<endl;
+            }
+             
+                temp=rugs[i];
+             
+           // cout<<ans<<" check \n"<<temp<<" ";
+        }
+        return ans;
+    }
+};
