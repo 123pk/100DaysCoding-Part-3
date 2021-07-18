28b5028131a41a2c6b752be6d872681cb64f18a3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-18 18:32:30 +0530

    Create Add minimum number of rungs.cpp

---
+++ Day29/Add minimum number of rungs.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode weekly contest 250
+Approach :- if the difference between two adjacent values of array is more than 'dist' then we need to add ((A[i]-A[i-1])-1)/dist  rungs to it
+*/
