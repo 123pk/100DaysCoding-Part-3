506e42c5949cd2229a76af86da35ade1c4390363
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-19 17:13:24 +0530

    Create D - Remove one element.cpp

---
+++ Day30/D - Remove one element.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Roun 605 Div 3
+Approach :- for a index 'i' we try to find the max length of increasing subarray till 'i' and max length of increasing subarray till 'i+2' such that A[i]<A[i+2]
+*/
