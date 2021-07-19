089e8a68ae9c62e4e51c7267ff472199627e3caf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-19 17:20:02 +0530

    Create D -  Alice bob and candies.cpp

---
+++ Day30/D -  Alice bob and candies.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 640 Div 4
+Approach :- Bruteforce, take care of previous value eaten and try to find if we can have candies of sum greater than that value of not.
+*/
