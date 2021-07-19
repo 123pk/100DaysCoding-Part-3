1baed4e837771f1bbfa21000564ca8650a9b3873
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-19 17:22:56 +0530

    Create G - Special Permutation.cpp

---
+++ Day30/G - Special Permutation.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 640 Div 4
+Approach :- we need to find permutaion of 'n' such that adjacent element has difference between 2 and 4 inclusive , we can always do so by printing all odd values <=n
+            and then 4 2 and rest of even number .
+            If 'n'<=3 it is not possible
+*/
