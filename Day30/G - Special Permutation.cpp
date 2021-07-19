4d5673d9f86f7d2eaf90c0cb1a629b90c644cc25
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-19 17:23:11 +0530

    Update G - Special Permutation.cpp

--- Day30/G - Special Permutation.cpp
+++ Day30/G - Special Permutation.cpp
@@ -5,3 +5,37 @@ Approach :- we need to find permutaion of 'n' such that adjacent element has dif
             and then 4 2 and rest of even number .
             If 'n'<=3 it is not possible
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        if(n<=3)cout<<"-1\n";
+        else{
+            int lo=n,le=n;
+            if(n%2==0)lo=n-1;
+            else le=n-1;
+            
+            while(lo>0){
+                cout<<lo<<" ";
+                lo-=2;
+            }
+            
+            cout<<"4 2 ";
+            int z=6;
+            while(z<=le){
+                cout<<z<<" ";
+                z+=2;
+            }
+            cout<<"\n";
+        }
+    }
+}
