c0245510aa5df18095d8c77706017d8baefa582b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-19 17:09:55 +0530

    Create C - Yet another broken keyboard.cpp

---
+++ Day30/C - Yet another broken keyboard.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 603 Div 3
+Approach :- we will try to find all substring which can be typed and are not overlapping , then if size of substring is 'n' the number of of substring that
+            can be typed is (n*(n+1))/2
+*/
