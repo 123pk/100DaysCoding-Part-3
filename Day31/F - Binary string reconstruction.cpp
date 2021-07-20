40f6fe3ebae927c9ff181da3bf714604915f9f06
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-20 22:22:52 +0530

    Create F - Binary string reconstruction.cpp

---
+++ Day31/F - Binary string reconstruction.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 640 Div 4
+Approach :- Greedily first construct string with patter '00' then add pattern for '11' and at the end of '01' or'10'
+*/
