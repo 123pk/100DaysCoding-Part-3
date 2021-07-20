e0482b8439185b2af883abe76586e6e7dbced86d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-20 23:03:34 +0530

    Create Number of provinces.cpp

---
+++ Day31/Number of provinces.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Number of provinces
+Approach :- create a graph using the matrix and do bfs from i=1 to n if the node is not already visited increment your count by 1 , during bfs visit all the nodes of that 
+            graph and mark them visited and return your count at the end.
+*/
