b75356eded9814c946038246b6448e6f395bb95c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-21 21:09:03 +0530

    Create Lowest common ancestor in binary search tree.cpp

---
+++ Day32/Lowest common ancestor in binary search tree.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Lowest common ancestor
+Approach :- The first node that is greater than both the elements is our lca for those nodes in binary search tree , do bfs and as soon as you find the above condition
+            break and return the value
+*/
