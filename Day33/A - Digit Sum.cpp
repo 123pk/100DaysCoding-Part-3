6c9ad0a00ee96b97bd2e3df6b4fa3288d8d61491
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-22 23:04:12 +0530

    Update A - Digit Sum.cpp

--- Day33/A - Digit Sum.cpp
+++ Day33/A - Digit Sum.cpp
@@ -3,3 +3,21 @@ Platform :- Codeforces
 Contest :- Harbour.Space Scholarship Contest 2021-2022 (open for everyone, rated, Div. 1 + Div. 2)
 Approach :- there are always 'n/10' such values of 'x' except for case when last digit of 'n' is 9 , in this case we have 'n/10 +1'
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        
+         
+            long long ans=n/10;
+            if((n%10)==9)ans++;
+            cout<<ans<<"\n";
+        
+    }
+}
