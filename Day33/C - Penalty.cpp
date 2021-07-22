f89c40d9d516f2315d5a7ebce3a5391f7c42b9f5
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-22 23:57:35 +0530

    Create C - Penalty.cpp

---
+++ Day33/C - Penalty.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Codeforces
+Contest :- 
+Approach :- we have 3 cases 
+           case 1 :- both players score in '?' situation
+           case 2:- player 1 scored in '?' and player 2 don't
+           case 3:- player 2 scored in '?' and player 1 don't 
+           out of all three the minimum games required is our answer
+*/
