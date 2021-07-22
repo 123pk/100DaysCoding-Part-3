d8a9f52d8515fa6bbfbab5e88859b167b69cf7f0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-22 23:03:54 +0530

    Create A - Digit Sum.cpp

---
+++ Day33/A - Digit Sum.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Harbour.Space Scholarship Contest 2021-2022 (open for everyone, rated, Div. 1 + Div. 2)
+Approach :- there are always 'n/10' such values of 'x' except for case when last digit of 'n' is 9 , in this case we have 'n/10 +1'
+*/
