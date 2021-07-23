88b0800157f60f098ff16735c7050d64cbe4b669
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-23 23:18:46 +0530

    Create B1 -  wonderful coloring 1.cpp

---
+++ Day34/B1 -  wonderful coloring 1.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 734 Div 3
+Problem :- B1 - wonderful coloring
+Approach :- if frequenecy of the character in string is >=2 we can color one of this character to 'R' and one to 'G' and out of all other characters of frequny 1 let the sum
+            count be 'c' then we can color 'c/2' additional ones 
+*/
