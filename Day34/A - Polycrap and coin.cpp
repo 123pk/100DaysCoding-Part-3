43ba6f43db1280c06d506dd8b758da9aed1b2a5c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-23 23:49:36 +0530

    Update A - Polycrap and coin.cpp

--- Day34/A - Polycrap and coin.cpp
+++ Day34/A - Polycrap and coin.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Codeforces
-Contest :- Codeforces Round 734 Div 3
+Contest :- Codeforces Round 734 Div #3
 Approach :- n/3 is distributed to both 1 and 2 , if n%3 we increment first if n%3==1 and second if n%3==2
 */
 #include<bits/stdc++.h>
