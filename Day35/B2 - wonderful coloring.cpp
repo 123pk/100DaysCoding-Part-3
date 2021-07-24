34699d9cc589fd4623635d33142a812645bd1bf8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-24 23:25:55 +0530

    Create B2 - wonderful coloring.cpp

---
+++ Day35/B2 - wonderful coloring.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 734 Div 3
+Approach :- we will find frequency of each elemnt of array and the frequecy of element is >=k then we will color 'k' of those with each of 'k' colors else if 
+           frequency is <k then we will create a vector where we will push 'c' times that element in array , at the end we will distribute equal number of elements between
+           'k' colors
+*/
