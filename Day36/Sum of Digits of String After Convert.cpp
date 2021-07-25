285bed4fa2e2d83e3675877449f283a498146a9d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-25 23:10:25 +0530

    Update Sum of Digits of String After Convert.cpp

--- Day36/Sum of Digits of String After Convert.cpp
+++ Day36/Sum of Digits of String After Convert.cpp
@@ -3,3 +3,28 @@ Platform :- Leetcode
 Problem :- Weekly contest 251
 Approach :- convert string into integer or numeric string accordingly and repeat the said operation k times
 */
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        map<char,int>P;
+        for(char ch='a';ch<='z';++ch){
+            P[ch]=(ch-'a')+1;
+        }
+        
+        string start="";
+        int tot=0;
+        for(auto &x:s)start+=to_string(P[x]);
+        tot=0;
+        for(auto &x:start)tot+=x-'0';
+        k--;
+        start=to_string(tot);
+   //     cout<<tot<<" ";
+        while(k){
+            tot=0;
+            for(auto &x:start)tot+=x-'0';
+            start=to_string(tot);
+            k--;
+        }
+        return tot;
+    }
+};
