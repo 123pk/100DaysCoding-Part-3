7452ef3d6e983d8c2dee5601b16ba34d04a47bd1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-25 23:15:01 +0530

    Create Largest Number After Mutating Substring.cpp

---
+++ Day36/Largest Number After Mutating Substring.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Problem :- Largest Number After Mutating Substring
+Approach :- run a loop for ( i=0 to i < size_of_array ) if we found a index whose replacement is greater than current value we replace with that value and from now onwards 
+            if replacement is >= current value we replace else break and return the changed string
+Contest :- Weekly contest 251
+*/
