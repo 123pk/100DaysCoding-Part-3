2affaf646fc75e162900373540ec6c4b6dbca91f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-25 23:15:16 +0530

    Update Largest Number After Mutating Substring.cpp

--- Day36/Largest Number After Mutating Substring.cpp
+++ Day36/Largest Number After Mutating Substring.cpp
@@ -5,3 +5,31 @@ Approach :- run a loop for ( i=0 to i < size_of_array ) if we found a index whos
             if replacement is >= current value we replace else break and return the changed string
 Contest :- Weekly contest 251
 */
+class Solution {
+public:
+    string maximumNumber(string num, vector<int>& change) {
+        string ans="";
+        int f=0,g=0;
+        for(auto &x:num){
+            if(change[x-'0']>=(x-'0')  && g==0){
+                if(f){
+                    ans+=to_string(change[x-'0']);
+                }
+                else{
+                    if(change[x-'0']>(x-'0')){
+                        f=1;
+                        ans+=to_string(change[x-'0']);
+                    }
+                    else ans+=to_string(change[x-'0']);
+                }
+                 
+                
+            }
+            else{
+                if(f)g=1;
+                ans+=x;
+            }
+        }
+        return ans;
+    }
+};
