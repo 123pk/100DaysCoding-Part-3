f8ad449352b24c4b52fbcaec525da75732158539
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-26 22:50:22 +0530

    Update Convert sorted array to binary search tree.cpp

--- Day37/Convert sorted array to binary search tree.cpp
+++ Day37/Convert sorted array to binary search tree.cpp
@@ -2,17 +2,7 @@
 Platform :- Leetcode
 Approach :- like merge sort we keep on breaking our array into smaller sub array and the choosing the middle of the and accordingly making it left child or right child
 */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     
