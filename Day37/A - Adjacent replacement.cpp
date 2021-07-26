1e18252ae93fa5019ed44f41755d1c9494c24744
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-26 21:39:43 +0530

    Update A - Adjacent replacement.cpp

--- Day37/A - Adjacent replacement.cpp
+++ Day37/A - Adjacent replacement.cpp
@@ -3,3 +3,26 @@ Platform :- Codeforces
 Contest :- Codeforces Round 498 Div 3
 Approach :- if A[i] is even then change A[i] to A[i]-1 for all ( 1 <= i <= n) 
 */
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int n;
+     cin>>n;
+     
+     long long A[n];
+     long long B[n];
+     for(int i=0;i<n;++i){
+         cin>>A[i];
+         
+     }
+     
+    for(auto &x:A){
+        if(x%2==0)cout<<(x-1)<<" ";
+        else cout<<x<<" ";
+    }
+    cout<<endl;
+     
+     
+}
