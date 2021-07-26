48dfdd49423266100fa7a7f9d1f5fdf6168f5ab6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-26 22:49:42 +0530

    Create Convert sorted array to binary search tree.cpp

---
+++ Day37/Convert sorted array to binary search tree.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- like merge sort we keep on breaking our array into smaller sub array and the choosing the middle of the and accordingly making it left child or right child
+*/
