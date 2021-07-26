3cab6bc2afd91d4af9d5c9917b72c7da1dd4f4ed
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-26 21:43:33 +0530

    Create B - Polycrap's practise.cpp

---
+++ Day37/B - Polycrap's practise.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 498 Div 3
+Approach :- store all index of occurence of each element in array, sort the array then choose the 'k' greatest element find its sum and their index respectively
+           sort the index in increasing order then find the size of each subarray formed 
+*/
