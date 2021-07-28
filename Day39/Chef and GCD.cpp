58b13840247b3963f58828fdc7558ca1c8176031
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-28 22:54:42 +0530

    Update Chef and GCD.cpp

--- Day39/Chef and GCD.cpp
+++ Day39/Chef and GCD.cpp
@@ -7,3 +7,30 @@ Approach :- We can clearly see that the max no of operations that we need to app
            Case 3 :- if we increment y--> (y+1) and gcd(x,y) >1 or vice versa then also we only have to do one operation
            Case 4: else we need 2 operations
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long x,y;
+        cin>>x>>y;
+        
+        if(__gcd(x,y)>1){
+            cout<<0<<"\n";
+            continue;
+        }
+        
+        
+        long long temp1=min(x,y),temp2=max(y,x);
+        int ans=2;
+        if(temp1%2==0 || temp2%2==0)ans=1;
+        else{
+            if(__gcd(temp2+1,temp1)>1 || __gcd(temp2,temp1+1)>1)ans=1;
+        }
+        
+        cout<<(ans)<<"\n";
+    }
+}
