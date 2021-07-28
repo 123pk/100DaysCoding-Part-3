17524d7a5c22fcc27899053524ae0ae92d0eb726
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-28 22:58:25 +0530

    Update CHFPLN.cpp

--- Day39/CHFPLN.cpp
+++ Day39/CHFPLN.cpp
@@ -2,4 +2,6 @@
 Platform :- Codechef
 Contest :- Codechef Cook off july 2021
 Problem :- Chef in infinite plane
+Approach :- We can analyse that for a given value of 'n' we can have atmax (n-1) disting points in 2-d plane so we will maintain a map to store frequecy of each element
+            we will increment our answer if frequecy of that element is less than the value of element
 */
