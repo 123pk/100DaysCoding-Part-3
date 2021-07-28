505a034eeb20dda094811d41baa587db2b07faf0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-28 23:09:10 +0530

    Update MODELQ.cpp

--- Day39/MODELQ.cpp
+++ Day39/MODELQ.cpp
@@ -1,4 +1,8 @@
 /*
 Platform :- Codechef
 Contest :- Codechef Cook off july 2021
+Approach :- We will find the min value of array and count total number of occurence in array , now we will run through array and will check if 'mn'*2>A[i] then we have
+            to change all the elements else we have to change 'n-d' elements only
+            here   d ---> frequency of minimum element    and mn ---> minimum element of array
+            
 */
