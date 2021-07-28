edb650628f5ef8d880b114eb241df26b9b344cb7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-28 23:09:26 +0530

    Update MODELQ.cpp

--- Day39/MODELQ.cpp
+++ Day39/MODELQ.cpp
@@ -6,3 +6,40 @@ Approach :- We will find the min value of array and count total number of occure
             here   d ---> frequency of minimum element    and mn ---> minimum element of array
             
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        long long mn=1e18;
+      
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            mn=min(mn,A[i]);
+        }
+        sort(A,A+n);
+        int f=0,d=0;
+     //   cout<<mn<<"check\n";
+        for(int i=0;i<n;++i){
+            if(A[i]==mn)d++;
+            else{
+                if((mn*2)>=(A[i])){
+                    f=1;break;
+                }
+            }
+        }
+        
+        if(f==0)cout<<(n-d)<<"\n";
+        else cout<<n<<"\n";
+    }
+}
