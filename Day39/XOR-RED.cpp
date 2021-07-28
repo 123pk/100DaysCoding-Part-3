584684717b5e2189c8f7b5a85e6238239f2aa1e1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-28 23:02:25 +0530

    Update XOR-RED.cpp

--- Day39/XOR-RED.cpp
+++ Day39/XOR-RED.cpp
@@ -1,4 +1,6 @@
 /*
 Platform :- Codechef
 Contest :- Codechef Cook off july 2021
+Approach :- We will store the string of each element , then for each bit we will check if ith bit is present in (n+1)/2 times atleast then we will add that bit to our X value
+            at the end we will change each values with A[i] xor X and find the OR value
 */
