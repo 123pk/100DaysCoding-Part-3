1b88eef528629312e4ce6efaed85fdf0f7da59be
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-29 23:18:47 +0530

    Create 0-1 matrix.cpp

---
+++ Day40/0-1 matrix.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Problem :- 0-1 matrix
+Approach :- push all the points {i,j} in queue where mat[i][j] = 0 and perform bfs and update the neigbouring cells with the min values out of all possible values   
+*/
