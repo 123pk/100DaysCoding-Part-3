34c399ef03b7929642d0aa52a47e04fa6e535634
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-30 23:22:33 +0530

    Update A - Pizzaforces.cpp

--- Day41/A - Pizzaforces.cpp
+++ Day41/A - Pizzaforces.cpp
@@ -1,7 +1,7 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Educational Round 112
-Approach :- if n is odd make n even n--> n+1 , as for n>6 we can acheive every even number and cost for acheiving is 15 if n<=6 and n*5/2 otherwise
+Approach :- if n is odd make n even n--> n+1 , as for n>6 we can acheive every even number and cost for acheiving is 15 if n<=6 and n*5/2 otherwise.
 */
 #include<bits/stdc++.h>
 using namespace std;
