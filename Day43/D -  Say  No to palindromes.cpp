d05b62f92fe37c4df57803c64de3e1e54100771c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-31 22:12:06 +0530

    Update D -  Say  No to palindromes.cpp

--- Day43/D -  Say  No to palindromes.cpp
+++ Day43/D -  Say  No to palindromes.cpp
@@ -6,3 +6,54 @@ Approach :- just '3' characters 'a', 'b' and 'c' are in string so we can find al
            there are six such permutation we will find the minimum number of changes need for each permutaiton for each index and we can find minimum 
            value of ( change[r]-change[l-1] ) for all the six permutation
 */
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	 
+		int n,q;
+		cin>>n>>q;
+		
+		string s;
+		cin>>s;
+		
+		vector<string>P={"abc","acb","bca","bac","cab","cba"};
+		vector<vector<int>>Q;
+		for(int i=0;i<P.size();++i){
+		    int c=0;
+		    vector<int>temp(n);
+		    for(int j=0;j<n;++j){
+		        if(s[j]!=P[i][j%3])c++;
+		        temp[j]=c;
+		    }
+		    Q.push_back(temp);
+		}
+		
+		for(int i=0;i<q;++i){
+		    int l,r;
+		    cin>>l>>r;
+		    
+		    l--;
+		    r--;
+		    int ans=INT_MAX;
+		    if(l==0){
+		        
+		        for(int j=0;j<Q.size();++j){
+		            int temp=Q[j][r];
+		            ans=min(ans,temp);
+		        }
+		         
+		    }
+		    else{
+		        for(int j=0;j<Q.size();++j){
+		            int temp=Q[j][r]-Q[j][l-1];
+		            ans=min(ans,temp);
+		        }
+		    }
+		    
+		    cout<<ans<<"\n";
+		}
+	 
+}
