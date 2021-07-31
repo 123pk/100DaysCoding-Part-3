a1859f840ba0e0b3ef72a81ab3befab076a74228
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-31 22:32:55 +0530

    Update D - Quering Multiset.cpp

--- Day43/D - Quering Multiset.cpp
+++ Day43/D - Quering Multiset.cpp
@@ -4,3 +4,47 @@ Contest :- D - Quering Multiset
 Approach :- we will use min heap/priority queue of pair ordered by first elment , where our first elment will be our intital value and second element will be value 
             that have been added before that element , every time we are asked to print the value we will choose element with smaller ( fist-second)value .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+struct comp {
+    constexpr bool operator()(
+        pair<long  long, long long> const& a,
+        pair<long long, long long> const& b)
+        const noexcept
+    {  
+        return a.first-a.second > b.first-b.second;
+    }
+};
+
+int main(){
+    int q;
+    cin>>q;
+    
+    priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,comp>P;
+    long long temp=0;
+    for(int i=1;i<=q;++i){
+        int x;
+        cin>>x;
+        
+        if(x==1){
+            long long y;
+            cin>>y;
+            
+            P.push({y,temp});
+        }
+        else if(x==2){
+            long long y;
+            cin>>y;
+            temp+=y;
+        }
+        else{
+            pair<long long,long long> tp = P.top();
+            P.pop();
+            tp.first+=temp;
+            tp.first-=tp.second;
+            cout<<tp.first<<"\n";
+        }
+    }
+    
+}
