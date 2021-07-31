d3faef9f56cf1e6d26824493684e51bb3594a21d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-31 22:37:02 +0530

    Update D - Quering Multiset.cpp

--- Day43/D - Quering Multiset.cpp
+++ Day43/D - Quering Multiset.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Atcoder
-Contest :- D - Quering Multiset
+Contest :- Atcoder Beginner Contest 212
 Approach :- we will use min heap/priority queue of pair ordered by first elment , where our first elment will be our intital value and second element will be value 
             that have been added before that element , every time we are asked to print the value we will choose element with smaller ( fist-second)value .
 */
