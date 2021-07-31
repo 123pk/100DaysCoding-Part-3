293a13f240f1b5bf1227f9fca73537b34c8cadad
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-07-31 22:35:55 +0530

    Create C -  Min difference.cpp

---
+++ Day43/C -  Min difference.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner contest 212
+Approach :- If you carefully analyse the problem it is same as merging two sorted arrays ,  we need to sort given array first then we will find min differece
+            between adjacent element of differnt type ( different arrays )
+*/
