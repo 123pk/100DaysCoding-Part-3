a093d333f146a1822ea2ce306e57708290bb61a6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-01 23:03:28 +0530

    Update Count number of special subsequence.cpp

--- Day44/Count number of special subsequence.cpp
+++ Day44/Count number of special subsequence.cpp
@@ -4,3 +4,21 @@ Problem :- Count number of special subsequence
 Contest :- Leetcode Weekly contest 252
 Hint :- dp
 */
+class Solution {
+public:
+    int countSpecialSubsequences(vector<int>& nums) {
+        long long dp[3]={0,0,0};
+        long long mod=1e9+7;
+        for(auto &x:nums){
+            dp[x]+=dp[x];
+            if(x==0){
+                dp[x]++;
+            }
+            else{
+                dp[x]+=dp[x-1];
+            }
+            dp[x]%=mod;
+        }
+        return dp[2];
+    }
+};
