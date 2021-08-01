e4058343f4174d371b36735714f10ddf19963b6d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-01 22:53:30 +0530

    Update Maximum number of weeks for which you can work.cpp

--- Day44/Maximum number of weeks for which you can work.cpp
+++ Day44/Maximum number of weeks for which you can work.cpp
@@ -3,3 +3,17 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 252
 Approach :- It is the [ min( tot , 2*(tot-max_value) + 1 ) ] here tot --> sum of all elements and max_value ---> max value of array
 */
+class Solution {
+public:
+    long long numberOfWeeks(vector<int>& A) {
+         
+        int n=A.size();
+         sort(A.begin(),A.end(),greater<int>());
+        long long tot=0;
+        for(auto &x:A)tot+=x;
+        int i=0,j=n-1;
+         
+        long long ans=min(tot,2*(tot-A[0])+1);
+        return ans;
+    }
+};
