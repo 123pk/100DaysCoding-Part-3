0c49b8dc2c05e43ec162442f10a86de66ae45e30
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-01 22:53:13 +0530

    Update Three Divisors.cpp

--- Day44/Three Divisors.cpp
+++ Day44/Three Divisors.cpp
@@ -3,3 +3,16 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 252
 Approach :- Find all the factors of given number (unique) if they are equal to 3 return true else false
 */
+class Solution {
+public:
+    bool isThree(int n) {
+        set<int>P;
+        for(int i=1;i*i<=n;++i){
+            if(n%i==0){
+                P.insert(i);
+                P.insert(n/i);
+            }
+        }
+        return P.size()==3;
+    }
+};
