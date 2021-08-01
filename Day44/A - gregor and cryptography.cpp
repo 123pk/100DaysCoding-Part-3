8456a11f46b771d678adb27e5392979e44d6fdc3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-01 23:03:16 +0530

    Update A - gregor and cryptography.cpp

--- Day44/A - gregor and cryptography.cpp
+++ Day44/A - gregor and cryptography.cpp
@@ -3,3 +3,26 @@ Platform :- Codeforces
 Contest :- Codeforces Round 735 Div 2
 Approach :- find any root of n-1 and print that root and n-1
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        n-=1;
+        long long min;
+        for(long long i=2;i*i<=n;++i){
+            if(n%i==0){
+                min=i;
+                break;
+            }
+        }
+        
+        cout<<min<<" "<<n<<"\n";
+    }
+}
