8aacc530b111226bfad44c8b027ae27997818701
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-01 22:52:33 +0530

    Create Maximum number of weeks for which you can work.cpp

---
+++ Day44/Maximum number of weeks for which you can work.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 252
+Approach :- It is the [ min( tot , 2*(tot-max_value) + 1 ) ] here tot --> sum of all elements and max_value ---> max value of array
+*/
