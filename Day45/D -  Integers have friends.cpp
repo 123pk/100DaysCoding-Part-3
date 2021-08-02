2489aa2dbcde383d294ea10689b35f205a1be18c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-02 17:31:42 +0530

    Create D -  Integers have friends.cpp

---
+++ Day45/D -  Integers have friends.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 736 Div 2
+Hint :- Use segment tree or sparse table for speed up query time , we will use these data structure to find the gcd of range and for each index we will
+        find the max lenght of subarray which have gcd>1.
+*/
