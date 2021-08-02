1d9f0f98b41d91e1bb408be5c14f7ebe89fd53f3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-02 17:41:55 +0530

    Update B - Gregor and Pawn.cpp

--- Day45/B - Gregor and Pawn.cpp
+++ Day45/B - Gregor and Pawn.cpp
@@ -5,3 +5,56 @@ Approach :- itterate thorugh Gregor pawn and if it is '1' and enemy pawn is '0' 
            --> if current Gregor pawn is '1'  and if (i-1) pawn of enemy is '1' we choose that pawn else we check if (i+1) enemy pawn is '1' then we mark that pawn as
            used so that we cannot use that pawn again and increment our count.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        string s,p;
+        cin>>s>>p;
+        
+        int ans=0;
+        for(int i=0;i<n;++i){
+            if(p[i]=='1'){
+                if(s[i]=='0'){
+                    ans++;
+                }
+                else{
+                    if((i==0)){
+                        if(s[i+1]=='1'){
+                            s[i+1]='&';
+                            ans++;
+                        }
+                    }
+                    else if(i==n-1){
+                        
+                        if(s[i-1]=='1'){
+                            ans++;
+                            s[i-1]='&';
+                        }
+                    }
+                    else{
+                        if(s[i+1]=='1' || s[i-1]=='1'){
+                            if(s[i-1]=='1'){
+                                s[i-1]='&';
+                                ans++;
+                            }
+                            else{
+                                s[i+1]='&';
+                                ans++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        cout<<ans<<"\n";
+    }
+}
