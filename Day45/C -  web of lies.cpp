fa95ad1344ec1866508d918b59b2881a5f74d4a0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-02 17:59:05 +0530

    Update C -  web of lies.cpp

--- Day45/C -  web of lies.cpp
+++ Day45/C -  web of lies.cpp
@@ -11,3 +11,60 @@ Appraoch :- create a graph from input such that there is outgoing edge from ( mi
            Query type 3 :- print ( n - count_of_nodes_with_atleast_one_edge )
            
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     
+         int n,m;
+         cin>>n>>m;
+         
+         vector<int>P[n];
+         for(int i=0;i<m;++i){
+             int u,v;
+             cin>>u>>v;
+             u--;
+             v--;
+             P[min(u,v)].push_back(max(u,v));
+         }
+         
+         map<int,int>Q;
+         int d=0;
+         for(int i=0;i<n;++i){
+             Q[i]=P[i].size();
+             if(Q[i])d++;
+         }
+         
+         int q;
+         cin>>q;
+         for(int i=0;i<q;++i){
+             int x;
+             cin>>x;
+             
+             if(x==1){
+                 int u,v;
+                 cin>>u>>v;
+                  u--;
+                 v--;
+                 
+                 Q[min(u,v)]++;
+                 if(Q[min(u,v)]==1)d++;
+             }
+             else if(x==2){
+                 int u,v;
+                 cin>>u>>v;
+                 
+                 u--;
+                 v--;
+                 Q[min(u,v)]--;
+                 if(Q[min(u,v)]==0)d--;
+                 
+             }
+             else{
+                 cout<<(n-d)<<"\n";
+             }
+         }
+          
+         
+    
+}
