4c79d75fa5b10442fc3f9370db7282b3d70f836b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-03 21:05:55 +0530

    Create dynamic range sum query.cpp

---
+++ Day46/dynamic range sum query.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- CSES
+Approach :- Used segment tree to update and answer query efficiently
+Resources :- Blog on segment tree [ https://cp-algorithms.com/data_structures/segment_tree.html ]
+             Youtube [ https://www.youtube.com/watch?v=2FShdqn-Oz8 ]
+*/
