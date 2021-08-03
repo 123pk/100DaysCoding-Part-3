0e4ed8ec67ccde81d172d924117ba85af36ca9b2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-03 21:10:22 +0530

    Update Range xor query.cpp

--- Day46/Range xor query.cpp
+++ Day46/Range xor query.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- CSES
-Approach :- Used segment tree to update and answer query efficiently
+Approach :- I segment tree to answer query efficiently we can use sparse table too as this is static
 Resources :- Blog on segment tree [ https://cp-algorithms.com/data_structures/segment_tree.html ]
              Youtube [ https://www.youtube.com/watch?v=2FShdqn-Oz8 ]
 */
