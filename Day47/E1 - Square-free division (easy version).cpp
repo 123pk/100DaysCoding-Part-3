2e8423e771e2fbfac6c7b1dec977cf740fbe4a6d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-04 18:44:18 +0530

    Update E1 - Square-free division (easy version).cpp

--- Day47/E1 - Square-free division (easy version).cpp
+++ Day47/E1 - Square-free division (easy version).cpp
@@ -8,3 +8,114 @@ Approach :- do prime factorisation of each value and maintain a vector which con
             --> A perfect square prime factors have 'even' power
             ---> for finding prime efficientyly use seive
 */
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+bool isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+      
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n,k;
+        cin>>n>>k;
+        
+        int A[n];
+        int zz=0;
+         for(int i=0;i<n;++i){
+             cin>>A[i];
+             if(A[i]==A[0])zz++;
+         }
+        
+       if(zz==n){
+            cout<<n<<"\n";
+            continue;
+        }
+        int c=0,x=0;
+        map<vector<int>,int>P,Q2;
+        map<int,int>Q,R;
+        for(int i=0;i<n;++i){
+          
+            if(isPrime(A[i])){
+                if(P[{A[i]}]){
+                    c++;
+                    x=0;
+                    P=Q2;
+                    P[{A[i]}]++;
+                }
+                else{
+                    P[{A[i]}]++;
+                }
+            }
+            else{
+                 vector<int>fre;
+                int d=0;
+                 
+                while(A[i]%2==0){
+                    A[i]/=2;
+                    d++;
+                }
+                if(d%2)fre.push_back(2);
+                
+                d=0;
+                for(int j=3;j<=A[i];j+=2){
+                    d=0;
+                    while(A[i]%j==0){
+                        A[i]/=j;
+                        d++;
+                    }
+                    if(d%2)fre.push_back(j);
+                }
+                
+                if(fre.size()==0){
+                    if(x){
+                         c++;
+                         P=Q2;
+                    }
+                    else{
+                        x++;
+                    }
+                    
+                    continue;
+                }
+                
+                if(P[fre]){
+                    c++;
+                    x=0;
+                    P=Q2;
+                    P[fre]=1;
+                }
+                else{
+                    P[fre]++;
+                }
+                
+            }
+        }
+        c++;
+        cout<<c<<"\n";
+    }
+}
