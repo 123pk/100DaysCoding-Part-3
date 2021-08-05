e4ad024c5f1103bc0f93eb6237b085d6b5b1ddf7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-05 14:21:33 +0530

    Create subordinates.cpp

---
+++ Day48/subordinates.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- CSES
+Topic :- Trees ,BFS ,backtracking
+Approach :- create tree from given input now do bfs starting from node '1' and while backtracking compute number of subordinates of that particular node
+*/
