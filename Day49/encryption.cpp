e29b84ebabc2f9ad6e967592ce1b3788d05a8761
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-06 22:38:53 +0530

    Update encryption.cpp

--- Day49/encryption.cpp
+++ Day49/encryption.cpp
@@ -3,3 +3,61 @@ Platform :- Hackerrank
 Approach :- find n,m which are floor and ceil of lenght of string , if ( m*n < s.length() ) then we add 1 to n , now we bruteforce all the values in 2d character array
             where no of rows is 'n' and no of coloumn is 'm' and fill the matrix with s[k] , k++ , if k>=s.length then we let the cell empty
 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'encryption' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string encryption(string s) {
+    int m = ceil(sqrt(s.size()));
+    int n = floor(sqrt(s.size()));
+    
+    if((m*n)<s.size())n++;
+    
+    char ch[n][m];
+    int k=0;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            if(k<s.size()){
+                ch[i][j]=s[k];
+                k++;
+            }
+            else{
+                ch[i][j]='*';
+            }
+        }
+    }
+    
+    string ans;
+    for(int j=0;j<m;++j){
+        for(int i=0;i<n;++i){
+            if(ch[i][j]=='*')continue;
+            ans+=ch[i][j];
+        }
+        ans+=' ';
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = encryption(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
