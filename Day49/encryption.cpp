2683e16546c685ae35b57f67219952225916ff83
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-06 22:38:31 +0530

    Create encryption.cpp

---
+++ Day49/encryption.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Hackerrank
+Approach :- find n,m which are floor and ceil of lenght of string , if ( m*n < s.length() ) then we add 1 to n , now we bruteforce all the values in 2d character array
+            where no of rows is 'n' and no of coloumn is 'm' and fill the matrix with s[k] , k++ , if k>=s.length then we let the cell empty
+*/
