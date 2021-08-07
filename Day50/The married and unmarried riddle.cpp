ac2654d1d78ce5a6a0bb29aa7abf5d832f6fed7a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-07 21:07:08 +0530

    Update The married and unmarried riddle.cpp

--- Day50/The married and unmarried riddle.cpp
+++ Day50/The married and unmarried riddle.cpp
@@ -1,7 +1,7 @@
 /*
 Platform :- Codedrills
 Contest :- ICPC Prelims Amritapuri 2020
-Approach :- remove all the '?' from string and if in new string there is 'MP' then print "YES" else "NO"
+Approach :- remove all the '?' from string and if in new string there is 'MU' then print "YES" else "NO"
 */
 #include<bits/stdc++.h>
 using namespace std;
