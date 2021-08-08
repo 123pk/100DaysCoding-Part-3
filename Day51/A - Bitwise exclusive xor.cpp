e777d65a582e6634e0bf28781c90bbcb074f7981
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-08 21:31:55 +0530

    Create A - Bitwise exclusive xor.cpp

---
+++ Day51/A - Bitwise exclusive xor.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 213
+Approach :- It is simple property of xor ( X xor Y) = Z then ( X xor Z) = Y and we were given X and Z, we needed Y.
+*/
