5fd14716ec8f7a0e09d1d7ac5ba7b77f73d0756d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-08 21:37:16 +0530

    Update A - Bitwise exclusive xor.cpp

--- Day51/A - Bitwise exclusive xor.cpp
+++ Day51/A - Bitwise exclusive xor.cpp
@@ -3,3 +3,13 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 213
 Approach :- It is simple property of xor ( X xor Y) = Z then ( X xor Z) = Y and we were given X and Z, we needed Y.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long a,b;
+    cin>>a>>b;
+    
+    long long ans=a xor b;
+    cout<<ans<<"\n";
+}
