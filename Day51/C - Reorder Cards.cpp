680140891512ff4a7b9adbe4fe64103b0cbeaf28
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-08 21:34:03 +0530

    Create C - Reorder Cards.cpp

---
+++ Day51/C - Reorder Cards.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 213
+Approach :- sort A and B separately and for pair A[i] and B[i] and store the rank of A[i] and B[i] when you sort them and print the rank.
+*/
