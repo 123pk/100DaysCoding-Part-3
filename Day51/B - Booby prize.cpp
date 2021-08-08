1574acb2467ee6d723e1adadd1d13812d50dd5fa
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-08 21:33:29 +0530

    Create B - Booby prize.cpp

---
+++ Day51/B - Booby prize.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 213
+Approach :- Make vector of pair of array element and index and sort accoring to value of array element 
+            print the index of second last element is sorted in ascending order .
+*/
