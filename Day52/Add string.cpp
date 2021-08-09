16960bb2d4d14388aa885f8f0ad2ae7176471989
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-09 23:43:41 +0530

    Update Add string.cpp

--- Day52/Add string.cpp
+++ Day52/Add string.cpp
@@ -1,4 +1,5 @@
 /*
 Platform :- Leetcode
 Approach :- We can do like the basic addition we have learnt for the first time in our school using same concept of "carry" and moving from end to front .
+            Take i,j where i = num1.size()-1 & j = num2.size()-1 , we will run a [ while( i>=0 || j>=0 ) ] we do the addition and take care of carry 
 */
