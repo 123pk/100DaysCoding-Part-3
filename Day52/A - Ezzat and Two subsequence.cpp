59cf914fd63f247bf0fc7d34d7d0d2bd5db9592b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-09 23:06:20 +0530

    Create A - Ezzat and Two subsequence.cpp

---
+++ Day52/A - Ezzat and Two subsequence.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforce Round 737 Div 2
+Approach :- calculate the sum of all element and sort the array then run a loop from [ i = n-1 to 1] and find two value average of left subarray to current index [0 , i-1]
+            and right subarray [i,n-1] , calculate the sum of those subarrays and store the max value for all such 'i' in ans .
+*/
