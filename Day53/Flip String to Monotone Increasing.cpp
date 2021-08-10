58d01f43130064b73807def845279af647b7c334
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-10 22:23:29 +0530

    Update Flip String to Monotone Increasing.cpp

--- Day53/Flip String to Monotone Increasing.cpp
+++ Day53/Flip String to Monotone Increasing.cpp
@@ -5,3 +5,25 @@ Approach :- let your answer be [ min( one ,zero ) ] where (one = no_of_one_in_st
             We we use two more value e,f which will take count of number of one's till that index and number of '0' till that index we can find number of '0' to left and '1' to 
             the right easily.
 */
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int c=0,d=0;
+        for(auto &x:s){
+            if(x=='0')c++;
+            else d++;
+        }
+        
+        int ans=min(c,d);
+        int e=0,f=0;
+        for(int i=0;i<s.size();++i){
+            if(s[i]=='1')f++;
+            else e++;
+            
+            int temp=(f+(c-e));
+            ans=min(ans,temp);
+        }
+        
+        return ans;
+    }
+};
