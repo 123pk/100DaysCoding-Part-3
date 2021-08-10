e6d70214423d6356f74ffb9dc4bd646cc65ee328
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-10 22:21:39 +0530

    Create Flip String to Monotone Increasing.cpp

---
+++ Day53/Flip String to Monotone Increasing.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- let your answer be [ min( one ,zero ) ] where (one = no_of_one_in_string ) and ( zero = no_of_zero_in_string ) , now run a loop and and for each index find the cost 
+            if we can make all the value to left of that index to be '0' and all the values to right to be '1' .
+*/
