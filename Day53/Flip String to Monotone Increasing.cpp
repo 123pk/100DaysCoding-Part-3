48b68d23e310e0f6aeca61c7c16d2f6627a27ce0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-10 22:23:21 +0530

    Update Flip String to Monotone Increasing.cpp

--- Day53/Flip String to Monotone Increasing.cpp
+++ Day53/Flip String to Monotone Increasing.cpp
@@ -2,4 +2,6 @@
 Platform :- Leetcode
 Approach :- let your answer be [ min( one ,zero ) ] where (one = no_of_one_in_string ) and ( zero = no_of_zero_in_string ) , now run a loop and and for each index find the cost 
             if we can make all the value to left of that index to be '0' and all the values to right to be '1' .
+            We we use two more value e,f which will take count of number of one's till that index and number of '0' till that index we can find number of '0' to left and '1' to 
+            the right easily.
 */
