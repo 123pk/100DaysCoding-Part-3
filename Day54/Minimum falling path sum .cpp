1edd5f92a3c372180f9e406f678508a98f553b2c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-11 22:46:47 +0530

    Update Minimum falling path sum .cpp

--- Day54/Minimum falling path sum .cpp
+++ Day54/Minimum falling path sum .cpp
@@ -6,3 +6,38 @@ Approach :- we will create a new grid with same dimension as of input grid and f
             
             At the end we will check all the value in last row and return the minimum value
 */
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<vector<int>>dist(n,vector<int>(m,1000));
+        
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                
+                if(i==0){
+                    dist[i][j] = matrix[i][j];
+                    continue;
+                }
+                dist[i][j] = min(dist[i-1][j]+matrix[i][j],dist[i][j]);
+                
+                if((j+1)<m){
+                    dist[i][j] = min(dist[i-1][j+1]+matrix[i][j],dist[i][j]);
+                }
+                
+                if((j-1)>=0){
+                    dist[i][j] = min(dist[i-1][j-1]+matrix[i][j],dist[i][j]);
+                }
+            }
+        }
+        
+        int ans = INT_MAX;
+        for(int i=0;i<m;++i){
+            ans = min(ans,dist[n-1][i]);
+        }
+        return ans;
+    }
+};
