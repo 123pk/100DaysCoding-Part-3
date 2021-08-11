8995930fbab52cf3c55b4aeb3f7eff311fa4ac95
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-11 23:27:55 +0530

    Update Array of doubled pair.cpp

--- Day54/Array of doubled pair.cpp
+++ Day54/Array of doubled pair.cpp
@@ -7,3 +7,38 @@ Approach :- sort the array in descending order , now there are two cases
               --> then  look for 2*x , double of this element P[x] <=P[2*x] , if this condition is not satisfied return false
            if you come out of loop then all your condition are satisfied and return true
 */
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        
+        map<int,int>P;
+        for(auto &x:arr){
+            P[x]++;
+        }
+        sort(arr.begin(),arr.end(),greater<int>());
+        for(auto&x:arr){
+            if(P[x]){
+             
+                if(x<0){
+                    int temp=x*2;
+                    if(P[x]<=P[temp]){
+                        P[temp]-=P[x];
+                        P[x]=0;
+                    }
+                    else return false;
+                    continue;
+                }
+                
+                if(x%2)return false;
+                
+                int temp=x/2;
+                if(P[x]<=P[temp]){
+                    P[temp]-=P[x];
+                    P[x]=0;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
