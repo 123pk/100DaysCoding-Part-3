0852dfa5b8e64daada96128289a74df368dbb464
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-14 10:16:49 +0530

    Create BFS shortest reach.cpp

---
+++ Day56/BFS shortest reach.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Hackerrank
+Approach :- We do basic bfs and for node 'x' let the 'c' be the cost of reach this node , now for all the adjacent nodes to 'x' cost of reaching out them is 'c+6' according
+            to problem statement. so we do basic bfs and starting node has cost '0' and for each node 'x' in bfs we add +6 to unvisited adjacent node od 'x' 
+*/
