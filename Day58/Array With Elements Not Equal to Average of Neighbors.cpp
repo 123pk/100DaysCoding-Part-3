6bca71a63912c91e716ee0348b78583dc52fa3cf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-15 23:27:51 +0530

    Update Array With Elements Not Equal to Average of Neighbors.cpp

--- Day58/Array With Elements Not Equal to Average of Neighbors.cpp
+++ Day58/Array With Elements Not Equal to Average of Neighbors.cpp
@@ -4,3 +4,14 @@ Contest :- Leetcode Weekly contest 254
 Appraoch :- All elements are distinct so we can exploit this property , we can always have that array , sort the array and for all the odd index( '0' based indexing) swap with 
             previous element
 */
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        for(int i=1;i<nums.size();i+=2){
+            swap(nums[i],nums[i-1]);
+        }
+        return nums;
+    }
+};
