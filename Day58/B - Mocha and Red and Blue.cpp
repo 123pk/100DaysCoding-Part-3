3a647e3e2f49bd3c056a62e7632f54ca497000aa
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-15 23:50:54 +0530

    Update B - Mocha and Red and Blue.cpp

--- Day58/B - Mocha and Red and Blue.cpp
+++ Day58/B - Mocha and Red and Blue.cpp
@@ -3,3 +3,62 @@ Platform :- Codeforces
 Contest :- Codeforces Round 738 Div 2
 Approach :- traverse through string if you found a '?' check to it's nearest value !='?' and change this value accordingly
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         int n;
+         cin>>n;
+         
+         string s;
+         cin>>s;
+         
+         if(count(s.begin(),s.end(),'?')==n){
+             
+             for(int i=0;i<n;++i){
+                 if(i%2)cout<<"R";
+                 else cout<<"B";
+             }
+             cout<<"\n";
+             continue;
+         }
+         
+         int f=0;
+         int last=0;
+         for(int i=0;i<s.size();++i){
+             if(s[i]!='?'){
+                 last=i;
+               //  cout<<last<<" ";
+                 char ch;
+                 if(s[i]=='B')ch='R';
+                 else ch='B';
+                 for(int j=i-1;j>=0;--j){
+                     if(s[j]=='?'){
+                         s[j]=ch;
+                         if(ch=='B')ch='R';
+                         else ch='B';
+                     }
+                     else break;
+                 }
+             }
+             else{
+                 if(i==n-1){
+                     char ch;
+                     if(s[last]=='B')ch='R';
+                     else ch='B';
+                     
+                     for(int j=last+1;j<n;++j){
+                         s[j]=ch;
+                         if(ch=='R')ch='B';
+                         else ch='R';
+                     }
+                 }
+             }
+         }
+        cout<<s<<"\n";
+    }
+}
