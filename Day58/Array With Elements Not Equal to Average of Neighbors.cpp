cdc748e28c237299279ee792d0edee6af0ca2b32
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-15 23:27:35 +0530

    Create Array With Elements Not Equal to Average of Neighbors.cpp

---
+++ Day58/Array With Elements Not Equal to Average of Neighbors.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly contest 254
+Appraoch :- All elements are distinct so we can exploit this property , we can always have that array , sort the array and for all the odd index( '0' based indexing) swap with 
+            previous element
+*/
