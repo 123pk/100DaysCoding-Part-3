b6ff54bf7c12894417fd57ca88b5f4a1c8b0cacd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-16 01:19:05 +0530

    Update D - Mocha and Hiking.cpp

--- Day59/D - Mocha and Hiking.cpp
+++ Day59/D - Mocha and Hiking.cpp
@@ -7,3 +7,62 @@ Approach :- i) If A[0] = 1 , then we have a valid rount n+1 --> 1 --> 2-->3...->
             n --> n+1  so we will print 1..n+1 in that case
 
 */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+         
+        vector<int>A(n);
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+        }
+        
+        if(A[0]==1){
+            cout<<(n+1)<<" ";
+            for(int i=1;i<=n;++i)cout<<i<<" ";
+            cout<<"\n";
+            continue;
+        }
+        
+        if(A[n-1]==0){
+            for(int i=1;i<=n+1;++i){
+                cout<<i<<" ";
+            }
+            cout<<"\n";
+            continue;
+        }
+        
+        int f=0;
+        int start=0;
+        for(int i=1;i<n;++i){
+            if(A[i]==1 && A[i-1]==0){
+               f=1;
+                start=i-1;
+              
+                break;
+            }
+        }
+        
+        if(f==0){
+            cout<<"-1\n";
+            continue;
+        }
+        
+        for(int i=0;i<n;++i){
+            if(i==start){
+                cout<<i+1<<" "<<n+1<<" ";
+            }
+            else cout<<(i+1)<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    
+}
