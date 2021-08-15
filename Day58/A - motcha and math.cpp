375fcd71b5516a607017b09c8d6f9e1ffd8cb6e4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-15 23:14:57 +0530

    Create A - motcha and math.cpp

---
+++ Day58/A - motcha and math.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 738 Div 2
+Approach :- Convert each value of array into binary string and maintain the frequency of each bit , now for each bit check if it appears 'n' times then ( ans+=pow(2,i))
+*/
