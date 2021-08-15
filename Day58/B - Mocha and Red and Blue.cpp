fd93ff124b8249a6938a5b55eb8640afa6a02adb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-15 23:50:31 +0530

    Create B - Mocha and Red and Blue.cpp

---
+++ Day58/B - Mocha and Red and Blue.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 738 Div 2
+Approach :- traverse through string if you found a '?' check to it's nearest value !='?' and change this value accordingly
+*/
