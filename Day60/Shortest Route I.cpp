c8d2b241a7e1f5812059e0216e5f8777655e238e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-16 23:17:16 +0530

    Update Shortest Route I.cpp

--- Day60/Shortest Route I.cpp
+++ Day60/Shortest Route I.cpp
@@ -1,4 +1,5 @@
 /*
 Platform :- CSES
 Approach :- standard Dijkstra algorithm implementation for finding minimum distance path between 1 to n
+            take account of used nodes and don't use them again and again, to avoid tle
 */
