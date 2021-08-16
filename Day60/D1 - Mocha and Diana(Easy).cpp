a9f980315116b810203cc8c114ef27761873a069
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-16 23:14:30 +0530

    Create D1 - Mocha and Diana(Easy).cpp

---
+++ Day60/D1 - Mocha and Diana(Easy).cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 738 Div 2
+Appraoch :- Find all the disconnected components of graph and for mocha and diana store the starting node of each connected component in set and now traverse through both the 
+            set and if the two values 'x' of mocha set and 'y' of diana set are not connected ,connect them and add the pair to your answer and respectively change the parent or
+            or starting node
+*/
