509defafc9167083db9a9d0513800f507e5bcd43
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-17 20:43:04 +0530

    Update Range sum query immutable.cpp

--- Day61/Range sum query immutable.cpp
+++ Day61/Range sum query immutable.cpp
@@ -4,3 +4,23 @@ Approach :- we maintain a prefix sum array for given input and for each range qu
             case 1 :- 'L' = 0 in this case we return ( pref[R] )
             case 2 :- 'L'!=0 in this case we return ( pref[R]-pref[L-1] )
 */
+class NumArray {
+public:
+    vector<int>temp,pref;
+    NumArray(vector<int>& nums) {
+        temp = nums;
+        for(int i=0;i<nums.size();++i){
+            if(i==0)pref.push_back(temp[i]);
+            else pref.push_back(pref[i-1]+temp[i]);
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        if(left==0){
+            return pref[right];
+        }
+       // left--;
+        //right--;
+        return pref[right]-pref[left-1]; 
+    }
+};
