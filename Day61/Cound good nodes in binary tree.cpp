57b886cd6c6634015486f1d218bdb216fa5226eb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-17 20:41:12 +0530

    Update Cound good nodes in binary tree.cpp

--- Day61/Cound good nodes in binary tree.cpp
+++ Day61/Cound good nodes in binary tree.cpp
@@ -2,3 +2,22 @@
 Platform :- Leetcode
 Approach :- We do dfs (recursively) and maintain a variable 'mx' that stores the max value till that current node in that path and if ( 'mx'<= node->val ) we increment our count++
 */
+class Solution {
+public:
+    int count(TreeNode* node,int mx){
+        if(!node)return 0;
+        int d=0;
+        if(mx<=node->val){
+            mx = node->val;
+            d=1;
+        }
+        
+        
+        return  d+count(node->left,mx)+count(node->right,mx);
+         
+    }
+    int goodNodes(TreeNode* root) {
+        if(!root)return 0;
+        return count(root,root->val);
+    }
+};
