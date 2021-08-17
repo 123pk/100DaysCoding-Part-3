0cbbe1ceee49f3d7e54e572baf73ea7a0575ec68
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-17 20:38:47 +0530

    Create Cound good nodes in binary tree.cpp

---
+++ Day61/Cound good nodes in binary tree.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Leetcode
+Approach :- We do dfs (recursively) and maintain a variable 'mx' that stores the max value till that current node in that path and if ( 'mx'<= node->val ) we increment our count++
+*/
