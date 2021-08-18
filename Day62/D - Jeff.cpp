4c096fffcd4fc5acfaea05a24e417a4173ebca14
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-18 22:31:01 +0530

    Update D - Jeff.cpp

--- Day62/D - Jeff.cpp
+++ Day62/D - Jeff.cpp
@@ -3,3 +3,35 @@ Platform :- Codedrills
 Contest :- ICPC Gwalior Pune Regionals 
 Approach :- we calculate the minimum time required by jay to reach the 'n'th index and if this value is >=n then Jeff will eat Jay else not
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,start;
+        cin>>n>>start;
+       // start--;
+        
+        string s;
+        cin>>s;
+        
+        int i=0;
+        int time=0;
+        while(start<n){
+            if(s[start]=='.'){
+                time++;
+                start++;
+            }
+            else {
+                time+=2;
+                start++;
+            }
+        }
+        
+        if(time>=n)cout<<"JEFF\n";
+        else cout<<"JAY\n";
+    }
+}
