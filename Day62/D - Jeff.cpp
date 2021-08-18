79d6979f5108d3b44a0662310e6020f058ab3dbb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-18 22:30:41 +0530

    Create D - Jeff.cpp

---
+++ Day62/D - Jeff.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codedrills 
+Contest :- ICPC Gwalior Pune Regionals 
+Approach :- we calculate the minimum time required by jay to reach the 'n'th index and if this value is >=n then Jeff will eat Jay else not
+*/
