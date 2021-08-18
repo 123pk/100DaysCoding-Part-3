ec83443d8ed7acb780b396556fa1a7306c29dd7a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-18 22:34:30 +0530

    Update K - Vichitrödinger's Cat.cpp

--- Day62/K - Vichitrödinger's Cat.cpp
+++ Day62/K - Vichitrödinger's Cat.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Codedrills
 Contest :- ICPC Gwalior Pune Regionals 2020
-Approach :- sort the array and for each index i, the max lenght of such subsequence is( min(i, n-(i+1))*2 + 1)
+Approach :- sort the array and for each index i, the max lenght of such subsequence is( min(i, n-(i+1))*2 + 1) for repeating values the max our of all calculated values for their
+            index is taken.
 */
