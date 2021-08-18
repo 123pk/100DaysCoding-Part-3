4ac2b776ca7459559c7cc78b91f6aea397956553
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-18 22:33:26 +0530

    Create K - Vichitrödinger's Cat.cpp

---
+++ Day62/K - Vichitrödinger's Cat.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codedrills
+Contest :- ICPC Gwalior Pune Regionals 2020
+Approach :- sort the array and for each index i, the max lenght of such subsequence is( min(i, n-(i+1))*2 + 1)
+*/
