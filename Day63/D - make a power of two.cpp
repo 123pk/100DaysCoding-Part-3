7c4cbe06cce3393cb2ec3d49bf1d33475cc42cf1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-19 17:36:09 +0530

    Update D - make a power of two.cpp

--- Day63/D - make a power of two.cpp
+++ Day63/D - make a power of two.cpp
@@ -5,3 +5,37 @@ Approach :- As the max value of A=10^9 so max operation required to make it into
             which  are power of 2 till 10^18 in (log N time ---> 60 itterations) then we will find for each value the minimum digits need to be changed so and print the minimum 
             value.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        string temp =to_string(n);
+        int c=temp.size();
+       // cout<<c<<" checking \n";
+     
+        int ans = INT_MAX;
+        for(long long i=0;i<=60;++i){
+            long long z = pow(2,i);
+            string zz = to_string(z);
+            int d=0,k=0;
+            for(int  j=0;j<c && k<zz.size();++j){
+                if(temp[j]==zz[k]){
+                    k++;
+                }
+            }
+            int tot = c-k;
+          //  cout<<tot<<" check "<<temp<<" "<<zz<<" "<<c<<" "<<k<<endl;
+            tot+=(zz.size()-k);
+            ans=min(ans,tot);
+        }
+        cout<<ans<<"\n";
+    }
+}
