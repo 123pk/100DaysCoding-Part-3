3fea6202e12dd958899dfb4bfde97524cb4a369f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-19 17:51:05 +0530

    Update E - Polycrap and string transformation.cpp

--- Day63/E - Polycrap and string transformation.cpp
+++ Day63/E - Polycrap and string transformation.cpp
@@ -6,3 +6,78 @@ Approach :- reverse the string and store the index of each character when first 
             and choose the first 'x' elements and try to create the given string if we are not able to do so we print ("-1") else we print the string and order in which character 
             are choosen
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        
+        map<char,int>P,R;
+        for(int i=s.size()-1;i>=0;--i){
+            if(P[s[i]]==0)P[s[i]]=i+1;
+            R[s[i]]++;
+        }
+        string fin;
+        vector<pair<int,char>>Q;
+        for(auto x:P){
+            Q.push_back({x.second,x.first});
+        }
+        
+        sort(Q.begin(),Q.end());
+        int f=0;
+        int d=1;
+        int z=0;
+        for(auto &x:Q){
+            fin+=x.second;
+            if(R[x.second]%d){
+                f=1;break;
+            }
+            else{
+                R[x.second]/=d;
+                z+=R[x.second];
+            }
+            d++;
+        }
+        
+        if(f)cout<<"-1\n";
+        else{
+            string ans;
+            for(int i=0;i<z;++i){
+                ans+=s[i];
+                R[s[i]]--;
+            }
+            
+            for(char ch='a';ch<='z';++ch){
+                if(R[ch]){
+                    f=1;break;
+                }
+            }
+            
+            if(f)cout<<"-1\n";
+            else{
+                string rec;
+                rec=ans;
+                string temp=ans;
+                set<char>used;
+                for(int i=0;i<fin.size();++i){
+                    used.insert(fin[i]);
+                    for(int j=0;j<temp.size();++j){
+                        if(used.find(temp[j])==used.end()){
+                            rec+=temp[j];
+                        }
+                    }
+                }
+                
+                if(rec!=s)f=1;
+                
+                if(f)cout<<"-1\n";
+                else cout<<ans<<" "<<fin<<"\n";
+            }
+        }
+    }
+}
