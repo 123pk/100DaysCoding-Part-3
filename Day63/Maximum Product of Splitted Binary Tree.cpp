12d1da347c69110c495c738b1547a4fb1859c78c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-19 20:59:58 +0530

    Update Maximum Product of Splitted Binary Tree.cpp

--- Day63/Maximum Product of Splitted Binary Tree.cpp
+++ Day63/Maximum Product of Splitted Binary Tree.cpp
@@ -5,3 +5,48 @@ Approach :- First compute the total sum value of each node ,here we used bfs to 
             
             For avoidng TLE in c++ use long long to caculate the max value and while returning the value returun its mod.
 */
+int mod = 1e9+7;
+class Solution {
+public:
+    int dfs(TreeNode* node,long long& ans,long long tot){
+        if(!node)return 0;
+        
+        if(!node->left && !node->right)return node->val;
+        
+        //temp is the sum of nodes of subtree where current node is root
+        long long temp = node->val + dfs(node->left,ans,tot) + dfs(node->right,ans,tot);
+         
+        ans = max(ans,temp*(tot-temp));
+        return temp;
+        
+    }
+    
+    int maxProduct(TreeNode* root) {
+        long long tot=0;
+        if(!root)return tot;
+        
+        
+        //Approach is to find the total sum of nodes and then dfs and find the 
+        //sum of node for each subtree of that node
+        queue<TreeNode*>bfs;
+        bfs.push(root);
+        
+        while(!bfs.empty()){
+            int c = bfs.size();
+            for(int i=1;i<=c;++i){
+                TreeNode* temp = bfs.front();
+                bfs.pop();
+                tot+=temp->val;
+                if(temp->left){
+                    bfs.push(temp->left);
+                }
+                
+                if(temp->right)bfs.push(temp->right);
+            }
+        }
+        long long ans=root->val;
+        
+        dfs(root,ans,tot);
+        return ans%mod ;
+    }
+};
