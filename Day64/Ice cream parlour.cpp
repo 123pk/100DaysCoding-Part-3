a0f7efcc1a6898f832e38b9405aab27f10bf2d99
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-20 22:16:29 +0530

    Create Ice cream parlour.cpp

---
+++ Day64/Ice cream parlour.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Hackerrank
+Approach :- Store the vector of index of occurence of each value in array and now itterate through array and check if both  ( A[i] and target- A[i] )are present as they will add
+            up to 'target' value then print the index , if both are same then check if they are present in array more than once or not if yes print the first two index in 
+            increasing order.
+*/
