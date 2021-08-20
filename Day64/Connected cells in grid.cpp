36f83a22a0ee3015cf8bcb8dabc4509048c8205c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-20 22:11:54 +0530

    Create Connected cells in grid.cpp

---
+++ Day64/Connected cells in grid.cpp
@@ -0,0 +1,4 @@
+/*
+Platform :- Hackerrank
+Approach :- BFS on all the cells contanining 1's and are not visited yet in all 8 directions possible and maintain the count of total cells that are connected , reuturn max count 
+*/
