de305b814aa38f83ddb7ab416833410703530e68
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-21 10:08:16 +0530

    Create Insert Position.cpp

---
+++ Day65/Insert Position.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codedrills
+Contest :- Interview Practise 16
+Approach :- as all elements are distinct we will do binary search and if element is present we return the index where it is present else we return the index of just greater value 
+            if target is greater than all the values we return the size of array ,remember( 0 -based indexing)
+*/
