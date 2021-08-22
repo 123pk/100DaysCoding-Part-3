c746cad7d394508875cb389636e4ec66dbc0e98f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-22 20:21:01 +0530

    Create Plaindrome crossword.cpp

---
+++ Day66/Plaindrome crossword.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codingcompetitionwithgoogle
+Contest :- Google Kickstart Round E 2021
+Approach :- Do bfs on all the alphabetical cells in grid and find if it can be part of a palindrome vertically or  horizontally and fill the mirror side ( Opposite) if empty
+            and push that cell index in bfs queue .
+*/
