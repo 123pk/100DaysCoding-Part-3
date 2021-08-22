7a21531a13e78f3dc0129d50b7ade4f8eea58b3e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-22 22:59:38 +0530

    Update Plaindrome crossword.cpp

--- Day66/Plaindrome crossword.cpp
+++ Day66/Plaindrome crossword.cpp
@@ -2,7 +2,7 @@
 Platform :- Codingcompetitionwithgoogle
 Contest :- Google Kickstart Round E 2021
 Approach :- Do bfs on all the alphabetical cells in grid and find if it can be part of a palindrome vertically or  horizontally and fill the mirror side ( Opposite) if empty
-            and push that cell index in bfs queue .
+            and push that cell index in bfs queue.
 */
 #include<bits/stdc++.h>
 using namespace std;
