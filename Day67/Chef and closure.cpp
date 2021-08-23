b1a207ab9ec93419eeee8c55d2d86d7db6a1b756
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-23 22:49:21 +0530

    Update Chef and closure.cpp

--- Day67/Chef and closure.cpp
+++ Day67/Chef and closure.cpp
@@ -7,3 +7,32 @@ Approach :- If you will analyse the problem carefully you will find that we have
             Case 3 :- If we have more than two (-1) then we need atleast one (1) positive one else array cannot be closed
             Case 4 :- In all othe case we have a closed array
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+       int n;
+       
+       cin>>n;
+       
+       int A[n];
+       int c=0,d=0,e=0,f=0;
+       for(int i=0;i<n;++i){
+           cin>>A[i];
+           if(abs(A[i])>1)c++;
+           //P[A[i]]++;
+           if(A[i]==-1)f++;
+           if(A[i]>0)d++;
+       }
+       
+       if(c>1 || (c  && f))cout<<0<<"\n";
+       else {
+           if(f>1 && d==0)cout<<"0\n";
+           else cout<<"1\n";
+       }
+    }
+}
