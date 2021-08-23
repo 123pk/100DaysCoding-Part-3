6f498328274a5185109fd17aec619c56ddb4524e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-23 21:27:58 +0530

    Create Two sum IV.cpp

---
+++ Day67/Two sum IV.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- Do bfs on array and maintain a haspmap which store which element is present or not ,if we reach a node which has value 'x' then we check if (k-x) is already present
+            or not ,if it is present then we return true else we didn't found any such case then return false
+*/
