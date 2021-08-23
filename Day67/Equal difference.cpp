c677c037e09c404f2b0efaf2f89cbee0256387e6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-23 22:26:41 +0530

    Create Equal difference.cpp

---
+++ Day67/Equal difference.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- Codechef August Cook off
+Approach :- We can easily see that the maximum length of such subarray is the count of most frequent element , let 'x' be the maximum frequency of some element in array
+            then we need to remove (n-x) elements, take care of the case when we have x=1, in that case we need one more element to make pair.
+*/
