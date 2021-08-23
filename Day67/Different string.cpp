a4543124812d09371fe82d70b6559aa3b25ba546
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-23 22:37:19 +0530

    Create Different string.cpp

---
+++ Day67/Different string.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- Codechef August Cook off 2021
+Approach :- As there are 'n' string and all string are of length 'n' , we can exploit this constraint we can have a string ans which stores the 'ith' value opposite of 'ith' character
+           of string 'i'. For example let n=4, i = 3 , ith string  = "1010" ,here ith(3rd) character is '1' so we will add '0' to our answer ( opposite ) of ith character. 
+*/
