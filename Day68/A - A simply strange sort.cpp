bf04039f54c7ecf3c5726a9a64cd610ef7b7fc0a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-24 23:26:23 +0530

    Create A - A simply strange sort.cpp

---
+++ Day68/A - A simply strange sort.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 740 Div 2 Based on VK CUP
+Approach :- Bruteforce try swapping odd position and even position till the time arrray is not sorted
+*/
