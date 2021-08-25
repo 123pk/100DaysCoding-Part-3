345ad3542671e9be4c00d78a5552a6bdb5c15aa1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-25 20:27:40 +0530

    Update Sum of square numbers.cpp

--- Day69/Sum of square numbers.cpp
+++ Day69/Sum of square numbers.cpp
@@ -2,3 +2,22 @@
 Platform :- Leetcode
 Approach :- We can find all the squares('x^2') less than 'c' and check if (c-x^2) is a square or not .
 */
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        
+        int i=0;
+        while(1){
+            long long temp = i;
+            temp*=temp;
+            
+            if(temp>c)break;
+            long long z = c-temp;
+            long long x = sqrt(z);
+            x*=x;
+            if(x==z)return true;
+            i++;
+        }
+        return false;
+    }
+};
