b384cd0277a6534491789ad6a0873333143b5e92
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-26 23:31:49 +0530

    Update A - The Miracle and the sleeper.cpp

--- Day70/A - The Miracle and the sleeper.cpp
+++ Day70/A - The Miracle and the sleeper.cpp
@@ -3,3 +3,26 @@ Platform :- Codeforces
 Contest :- Codeforces Round 741 Div 2
 Approach :- let x = (r/2)+1 , if x>=l then ans = r%x else ans = r%l
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int  main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long l,r;
+        cin>>l>>r;
+        
+        long long temp=r/2;
+        temp++;
+        long long ans=0;
+        if(l<=temp){
+            ans+=(r%temp);
+        }
+        else{
+            ans+= r%l;
+        }
+        cout<<ans<<"\n";
+    }
+}
