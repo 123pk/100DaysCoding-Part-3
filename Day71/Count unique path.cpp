2e7a956dda4374d3ab3c9a43f887c62d88629009
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-27 22:31:28 +0530

    Update Count unique path.cpp

--- Day71/Count unique path.cpp
+++ Day71/Count unique path.cpp
@@ -3,3 +3,19 @@ Platform :- Codedrill
 Approach :- the number of ways to reach any cell in first row or first column is '1' , then for all other cell ( dp[i][j] = dp[i-1][j] + dp[i][j-1] ) total number of ways is
             dp[n-1][m-1]
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+class CountUniquePaths {
+public:
+	int getCount(int n, int m) {
+		// Code here
+		vector<vector<int>>dp(n,vector<int>(m,1));
+   for(int i=1;i<n;++i){
+       for(int j=1;j<m;++j){
+           dp[i][j] = dp[i-1][j]+dp[i][j-1];
+       }
+   }
+   return dp[n-1][m-1];
+	}
+};
