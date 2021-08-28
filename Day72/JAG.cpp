43428f3b09fb3d58541ee57ba801c6e9570696ad
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-28 23:50:48 +0530

    Create JAG.cpp

---
+++ Day72/JAG.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codechef
+Contest :- Codechef August Lunchtime 2021
+Approach :- We can analyse with some test caase that answer is either 'n' or '1' , we get 'n' when we have all the weight arranged in increasing order from 1 to n with difference 
+           W[i] - W[i-1] = 1, else we have '1' connected componenet
+*/
