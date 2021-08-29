e9a248095c989541cfc724d2219cccd4b3650bb3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-29 22:55:42 +0530

    Create A variety of operations .cpp

---
+++ Day73/A variety of operations .cpp
@@ -0,0 +1,10 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Deltix round 742 Div(1+2)
+Approach :- You just need to analyse that there are 3 cases :- 
+            Case 1:- Difference is odd --> not possible 
+            Case 2:- Even difference and a = b 
+                 ---> Case 2.1 a = 0  then answer is 0 as we don't need to perform any operation
+                 ---> Case 2.2 a != 0 then answer is 1
+            Case 3: else we need 2 operations 
+*/
