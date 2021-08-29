72b5b2b7caafbdf95871d91df2e10a3c56eb0b2f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-29 23:10:34 +0530

    Create Maximum Difference between highest and lowest of K scores.cpp

---
+++ Day73/Maximum Difference between highest and lowest of K scores.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Leetcode Weekly Contest 256
+Approach :- sort the array in increasing order and then find the difference between kth element from current index starting from k to n
+*/
