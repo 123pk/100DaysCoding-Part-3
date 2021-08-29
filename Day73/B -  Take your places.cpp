86653c5480035b631f77741e9fececddd5168d0b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-29 23:16:30 +0530

    Update B -  Take your places.cpp

--- Day73/B -  Take your places.cpp
+++ Day73/B -  Take your places.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Codeforces
-Contest :- Codeforces Round 742 Div(1+2)
+Contest :- Codeforces Deltix Round  Div(1+2)
 Approach :- let x = no of odd numbers , y = no of even numbers . If abs( x - y) > 1  then it is not possible to do so .
            --> else if ( x == y )  --> then we try to form two case oeoe.. and eoeo.. where o = odd number and e = evne number and find cost of both and store the min in answer
            ---> else if (x > y) then we try eoeo.. else we find cost of converting to oeoe..
