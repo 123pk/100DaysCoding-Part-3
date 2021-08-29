376856e60ae33a8651324430df0f22cc39c32b0b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-29 23:08:06 +0530

    Update Find Kth larges integer in array.cpp

--- Day73/Find Kth larges integer in array.cpp
+++ Day73/Find Kth larges integer in array.cpp
@@ -1,5 +1,5 @@
 /*
 Platform :- Leetcode
-Contest :- Leetcode Weekly Contest 
+Contest :- Leetcode Weekly Contest 256
 Approach :- sort the strings in lexiorgraphical order taking care of length ( smaller length string should come first ) then return the respective string
 */
