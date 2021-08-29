ba0651489902b1153ece0099c755b7c01dcf7790
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-29 23:08:35 +0530

    Update Find Kth larges integer in array.cpp

--- Day73/Find Kth larges integer in array.cpp
+++ Day73/Find Kth larges integer in array.cpp
@@ -3,3 +3,23 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly Contest 256
 Approach :- sort the strings in lexiorgraphical order taking care of length ( smaller length string should come first ) then return the respective string
 */
+bool comp(string &P,string &Q){
+    if(P.length()==Q.length()){
+        for(int i=0;i<P.length();++i){
+            if(P[i]>Q[i])return false;
+            if(P[i]<Q[i])return true;
+        }
+        return false;
+    }
+    return P.length()<Q.length();
+}
+
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        sort(nums.begin(),nums.end(),comp);
+        int n = nums.size();
+        k = n-k;
+        return nums[k];
+    }
+};
