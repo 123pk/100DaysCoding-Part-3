8757d4cfed8a1e2fa1c68bc47a081409b11a757c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-30 22:59:11 +0530

    Create C - Many Balls.cpp

---
+++ Day74/C - Many Balls.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder 
+Contest :- Atcoder Beginner Contest 216
+Approach :- we can go in reverse direction from 'n' to '0' , we can use stack and if ( n % 2 == 1) then we add 'B' in stack and ( n-- ) else we push 'A' and (n /= 2)
+            at the end we print all the value of stack .
+*/
