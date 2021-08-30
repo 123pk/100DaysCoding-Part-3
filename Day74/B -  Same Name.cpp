a3ee5476930dfd9ba3ba49783920b85a87ec812d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-30 22:56:11 +0530

    Create B -  Same Name.cpp

---
+++ Day74/B -  Same Name.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Atcoder 
+Contest :- Atcoder Beginner Contest 216
+Approach :- we can use set of pair to store the first and last name in pair and if we get a value which is already in set we print yes else no.
+*/
