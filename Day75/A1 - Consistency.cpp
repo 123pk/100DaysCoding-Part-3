47a6eeb2c21ad84daab5785618c851f6f19fa3da
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-31 22:26:11 +0530

    Update A1 - Consistency.cpp

--- Day75/A1 - Consistency.cpp
+++ Day75/A1 - Consistency.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Facebook.com/codingcompetitions
-Contest :- Facebook Hackercup
+Contest :- Facebook Hackercup 2021
 Approach :- Find the cost of converting all letters to one of the most frequent consonent and cost of converting all letters to most frequent vowel, and the min value of both is
             our answer
 */
