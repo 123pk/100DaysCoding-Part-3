862249a000127c8f56a1dc6697e46564d6d3493f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-08-31 22:25:31 +0530

    Create A2 - Consistency.cpp

---
+++ Day75/A2 - Consistency.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Facebook.com/codingcompetitions
+Contest :- Facebook Hackercup 2021 Qualification round
+Approach :- We will treat each character and node and the relation given as the edge between them then we will find the cost of coverting current nodes to all others which are 
+            reachable from this node using dfs/bfs and cost of each edge be '1' then we look for character which is reachable from all the characters and have minimum cost
+*/
