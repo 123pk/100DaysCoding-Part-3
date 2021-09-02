f90dc0e023ee352e3ec7ba147df84a8631d00f0f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-02 22:05:09 +0530

    Create Game with Array.cpp

---
+++ Day77/Game with Array.cpp
@@ -0,0 +1,6 @@
+/*
+Platform:- Codeforces
+Contest :- Codeforces Round #643 Div 2
+Approach :- if n==s we cann't find any such value and array ,and if ((s-(n-1))-(n-1)<2) condition is satisfied then also we cannot find any value and we need to print "NO"
+            else we print yes and out array is (n-1)number of 1's and one s-(n-1) and we cannot have a subarray with value (s-n)
+*/
