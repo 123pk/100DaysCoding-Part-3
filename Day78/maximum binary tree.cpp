fb5e6939037d29dfb1db3b0be8c1bf5e4e7f5c4d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-03 23:15:20 +0530

    Update maximum binary tree.cpp

--- Day78/maximum binary tree.cpp
+++ Day78/maximum binary tree.cpp
@@ -1,5 +1,10 @@
 /*
 Platform :- leetcode
+Approaach :- The key idea is:
+         1. We scan numbers from left to right, build the tree one node by one step;
+         2. We use a stack to keep some (not all) tree nodes and ensure a decreasing order;
+         3. For each number, we keep pop the stack until empty or a bigger number; The bigger number (if exist, it will be still in stack) is current number's root, 
+            and the last popped number (if exist) is current number's left child (temporarily, this relationship may change in the future); Then we push current number into the stack.
 */
 class Solution {
 public:
