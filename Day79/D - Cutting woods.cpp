b8f0f24d00a89cae32d51dda6efc03315bf1587e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-04 21:59:04 +0530

    Create D - Cutting woods.cpp

---
+++ Day79/D - Cutting woods.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 217
+Approach :- store each cut in set and for each 'x' find the lowerbound of 'x' in set , if lowe_bound is the first element of set then answer is that first value else 
+            find the difference of lowe_bound(x) and value just before the lower_bound(x) value.
+*/
