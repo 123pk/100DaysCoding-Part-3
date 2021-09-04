22ea5a9fa0fa76c6d909a067dce55f832fbf5204
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-04 21:56:13 +0530

    Update C - Inverse of permutaion .cpp

--- Day79/C - Inverse of permutaion .cpp
+++ Day79/C - Inverse of permutaion .cpp
@@ -3,3 +3,20 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 217
 Approach :- Bruteforce create a new array and make A[p[i]] = i;
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    vector<int>ans(n);
+    for(int i=0;i<n;++i){
+        int x;
+        cin>>x;
+        x--;
+        ans[x]=i+1;
+    }
+    for(auto x:ans)cout<<x<<" ";
+    cout<<endl;
+}
