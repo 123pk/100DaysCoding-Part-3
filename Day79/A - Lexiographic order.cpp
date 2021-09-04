c92d28efba370872af99ae17588cc3497828ccf3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-04 21:51:14 +0530

    Update A - Lexiographic order.cpp

--- Day79/A - Lexiographic order.cpp
+++ Day79/A - Lexiographic order.cpp
@@ -3,3 +3,13 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 217
 Approach :- just compare two string .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s,t;
+    cin>>s>>t;
+    
+    if(s<t)cout<<"Yes\n";
+    else cout<<"No\n";
+}
