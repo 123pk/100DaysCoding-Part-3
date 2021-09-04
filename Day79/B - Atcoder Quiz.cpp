c7c491bc98b3ce4051392dfd609496821475872e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-04 21:52:47 +0530

    Update B - Atcoder Quiz.cpp

--- Day79/B - Atcoder Quiz.cpp
+++ Day79/B - Atcoder Quiz.cpp
@@ -3,3 +3,21 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 217
 Approach :- Bruteforce
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    set<string>temp;
+    temp.insert("ABC");
+    temp.insert("ARC");
+    temp.insert("AGC");
+    temp.insert("AHC");
+    
+    string a,b,c;
+    cin>>a>>b>>c;
+    
+    temp.erase(a);
+    temp.erase(b);
+    temp.erase(c);
+    for(auto x:temp)cout<<x<<endl;
+}
