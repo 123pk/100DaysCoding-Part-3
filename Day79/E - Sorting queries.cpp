f14c3bd6ce36e929e5c0fd7c3dfcac74c48a0524
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-04 22:02:42 +0530

    Create E - Sorting queries.cpp

---
+++ Day79/E - Sorting queries.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 217
+Approach :- use queue to store the value in it and when we have to sore we use map to get all the elements of queue in the map with frequency and make queue empty , if queue is empty
+           we use all the values in map and decremetn it coutn ( if count becomes 0 we remove it ) and make it empty else we print the first value of queue and remove that value.
+*/
