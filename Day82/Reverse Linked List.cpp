288511bc21209f1cff761360d76d379e1dd1f9c8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-07 21:54:45 +0530

    Create Reverse Linked List.cpp

---
+++ Day82/Reverse Linked List.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Approach :- use three pointers one to point at previous element, one to point current element and one to point next element , make current element point to previous element 
+            keep on moving right and pointing to the previous element as soon as you reach the last element make head point to that last element and return head.
+*/
