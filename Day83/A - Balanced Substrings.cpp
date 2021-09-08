490007918026fb1c13cede46621098cfe7ee1bc7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-08 22:13:54 +0530

    Create A - Balanced Substrings.cpp

---
+++ Day83/A - Balanced Substrings.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educaional Round 113 Div 2
+Approach :- Find substring "ab" or "ba" in string if not present print -1 else print the start index and end index of this substring
+*/
