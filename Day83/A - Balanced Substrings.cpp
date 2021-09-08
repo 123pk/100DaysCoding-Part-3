6038ced920e2037bd3da1fde6797a5c17cac42ce
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-08 22:24:50 +0530

    Update A - Balanced Substrings.cpp

--- Day83/A - Balanced Substrings.cpp
+++ Day83/A - Balanced Substrings.cpp
@@ -1,7 +1,7 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Educaional Round 113 Div 2
-Approach :- Find substring "ab" or "ba" in string if not present print -1 else print the start index and end index of this substring
+Approach :- Find substring "ab" or "ba" in string if not present print -1 else print the start index and end index of this substring.
 */
 #include<bits/stdc++.h>
 using namespace std;
