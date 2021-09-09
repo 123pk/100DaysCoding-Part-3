ba5bfe3c247f58983a1b1dadd9a70802d647c788
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-09 22:57:37 +0530

    Update D - Phoenix and socks.cpp

--- Day84/D - Phoenix and socks.cpp
+++ Day84/D - Phoenix and socks.cpp
@@ -10,3 +10,116 @@ Approach :- If you will carefully read the  problem you will be able to distingu
                      if the amount of some unmatched socks is >1 then we add floor[x/2] to minimum value side.
                      and then it becomes case 1 and we solve in same way as we solve case 1.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int cost(map<int,int>left,map<int,int>right){
+    int ans=0;
+    
+    for(auto &x:left){
+        if(right[x.first]){
+            x.second-=min(x.second,right[x.first]);
+            if(x.second)ans+=x.second;
+        }
+        else ans+=x.second;
+    }
+    
+    return ans;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,l,r;
+        cin>>n>>l>>r;
+        
+        vector<int> A(n);
+        map<int,int>left,right;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            if(i<l)left[A[i]]++;
+            else right[A[i]]++;
+        }
+        
+        int ans=0;
+        if(l==r){
+            ans+=cost(left,right);
+        }
+        else{
+            //make l = = r;
+            priority_queue<pair<int,int>>P;
+            
+            for(auto& x:left){
+                if(right[x.first]){
+                    int temp=min(x.second,right[x.first]);
+                    right[x.first]-=temp;
+                    x.second-=temp;
+                }
+            }
+            
+            if(l>r){
+                for(auto x:left){
+                    P.push({x.second,x.first});
+                }
+                
+                while(r<l){
+                    pair<int,int>temp = P.top();
+                    P.pop();
+                    
+                    if(temp.first>1){
+                        int z  = temp.first/2;
+                        z = min(z,abs(l-r)/2);
+                        r+=z;
+                        l-=z;
+                        ans+=z;
+                        left[temp.second]-=z;
+                        right[temp.second]+=z;
+                    }
+                    else{
+                        ans++;
+                        r++;
+                        l--;
+                        left[temp.second]--;
+                        right[temp.second]++;
+                    }
+                }
+            }
+            else{
+                for(auto x:right){
+                    P.push({x.second,x.first});
+                }
+                while(r>l){
+                    pair<int,int>temp = P.top();
+                    P.pop();
+                    
+                    if(temp.first>1){
+                        int z  = temp.first/2;
+                        z = min(z,abs(l-r)/2);
+                        r-=z;
+                        l+=z;
+                        ans+=z;
+                        left[temp.second]+=z;
+                        right[temp.second]-=z;
+                    }
+                    else{
+                        ans++;
+                        r--;
+                        l++;
+                        left[temp.second]++;
+                        right[temp.second]--;
+                    }
+                }
+                
+               // for(auto x:left)cout<<x.first<<' '<<x.second<<"\n";
+                
+               // for(auto x:right)cout<<x.first<<' '<<x.second<<"\n";
+            }
+            
+            ans+=cost(left,right);
+        }
+        cout<<ans<<"\n";
+        
+    }
+}
