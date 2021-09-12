d00d97ee15e43fd6df45040c5d6581fa350043cb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-12 23:02:12 +0530

    Create Reverse Prefix of words.cpp

---
+++ Day87/Reverse Prefix of words.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Leetcode
+Contest :- Weekly contest 258
+Approach :- Find the first index of required character and reverse string upto that index and leave the rest of string as is and concatenate.
+*/
