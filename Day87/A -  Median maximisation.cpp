e71092e9d22793af7f4b72adbd890e996ceca267
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-12 22:54:16 +0530

    Create A -  Median maximisation.cpp

---
+++ Day87/A -  Median maximisation.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Global Round 16
+Approach :- for given value of 'n' fill floor((n)/2) values with '0' and for rest of x = (n-(n+1)/2)+1 , we will generate sum 's' which is floor(s/x)
+*/
