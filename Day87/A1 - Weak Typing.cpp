e65aca5e1e09d0a057b8100d2de4d54030c8143d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-12 22:41:47 +0530

    Create A1 - Weak Typing.cpp

---
+++ Day87/A1 - Weak Typing.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Facebook
+Contest :- Facebook Hackercup 2021 Round 1
+Approach :- remove all 'F' from the stiring and now you are left with 'O' and 'X', count all the points where we have 'OX' or 'XO' print the value which is maximum
+*/
