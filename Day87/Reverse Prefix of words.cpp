05f781f95ae4ac987e5c7009fd0636018883a935
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-12 23:02:38 +0530

    Update Reverse Prefix of words.cpp

--- Day87/Reverse Prefix of words.cpp
+++ Day87/Reverse Prefix of words.cpp
@@ -3,3 +3,25 @@ Platform :- Leetcode
 Contest :- Weekly contest 258
 Approach :- Find the first index of required character and reverse string upto that index and leave the rest of string as is and concatenate.
 */
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        string ans;
+        int idx=0;
+        for(int i=0;i<word.size();++i){
+            ans+=word[i];
+            if(word[i]==ch){
+                //ans+=ch;
+                idx=i+1;
+                break;
+            }
+        }
+        if(idx==0)return ans;
+       // if(idx)
+        reverse(ans.begin(),ans.end());
+        
+        for(int i=idx;i<word.size();++i)ans+=word[i];
+        
+        return ans;
+    }
+};
