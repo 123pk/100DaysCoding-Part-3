01b787153c028475265c69d91f0d421c498e9753
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-13 21:37:13 +0530

    Create D1 - Seating Arrangements.cpp

---
+++ Day88/D1 - Seating Arrangements.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Global Round 16
+Approach :- as n is 1 for each value we will try to find number of values which already seated , those values will have value less than current value we will use set to find such value
+            to take care of duplicates we will use map to store the frequency
+*/
