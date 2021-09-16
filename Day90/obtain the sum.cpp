2736e0fcb74435e5f320adf8c4e026fd38c48f86
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-16 21:27:41 +0530

    Update obtain the sum.cpp

--- Day90/obtain the sum.cpp
+++ Day90/obtain the sum.cpp
@@ -1,1 +1,6 @@
-
+/*
+Platform :- Codechef
+Contest :- Codechef starters 11
+Approach :- We  have array [1,2,3,4....n], The sum of 'n' elements for this type of array is (n*(n+1))/2 , if we will remove 's' from it we will get a value , let it be 'x' is ( 1 <= 'x' <= n) then print 'x' 
+            else  solution don't exist and print '-1'
+*/
