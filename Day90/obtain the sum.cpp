565d3969e857e49592997638314f606dd9ea9f78
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-16 21:27:54 +0530

    Update obtain the sum.cpp

--- Day90/obtain the sum.cpp
+++ Day90/obtain the sum.cpp
@@ -4,3 +4,23 @@ Contest :- Codechef starters 11
 Approach :- We  have array [1,2,3,4....n], The sum of 'n' elements for this type of array is (n*(n+1))/2 , if we will remove 's' from it we will get a value , let it be 'x' is ( 1 <= 'x' <= n) then print 'x' 
             else  solution don't exist and print '-1'
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n,s;
+        cin>>n>>s;
+        
+        long long z =n;
+        z*=(n+1);
+        z/=2;
+        z-=s;
+        long long ans=-1;
+        if(1<=z && z<=n)ans=z;
+        cout<<ans<<"\n";
+    }
+}
