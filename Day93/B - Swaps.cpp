5228d1e62878fb016163cf233c9777338dfbeff1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-18 22:15:41 +0530

    Update B - Swaps.cpp

--- Day93/B - Swaps.cpp
+++ Day93/B - Swaps.cpp
@@ -1,4 +1,7 @@
 /*
 Platform :- Codeforces
 Contest :- Codeforces Round 643 Div 2
+Approach :- For each value from 1 to 2n find the smallest index which contains A[i]+1 or higher . We can do so by maintaining an array of size 2n and an last pointer which points
+            the last value, if our current value is greater than or equal to last value then we update our index from ( last to B[i] ) . Now for each value from 1 to n we find 
+            ans = min(ans , value[A[i]+1] + i)
 */
