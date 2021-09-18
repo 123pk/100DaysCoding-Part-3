eb94700c19300ae7d325b31bdf129db764f13bf0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-18 22:09:05 +0530

    Update A - Count down.cpp

--- Day93/A - Count down.cpp
+++ Day93/A - Count down.cpp
@@ -1,4 +1,6 @@
 /*
 Platform :- Codeforces 
 Contest :- Codeforces Round 743 Div 2
+Approach :- Basic approach is to take all the non zero elements to the digit position and make them zero by decrementing them by '1' in one operation , which is equal to the sum
+            of all digits and total  number of indexes which contain non zero digit .  We don't count the digit index .
 */
