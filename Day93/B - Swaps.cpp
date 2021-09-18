ff8676a48301450ce9141206450a5fd81df2932a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-18 22:15:52 +0530

    Update B - Swaps.cpp

--- Day93/B - Swaps.cpp
+++ Day93/B - Swaps.cpp
@@ -5,3 +5,55 @@ Approach :- For each value from 1 to 2n find the smallest index which contains A
             the last value, if our current value is greater than or equal to last value then we update our index from ( last to B[i] ) . Now for each value from 1 to n we find 
             ans = min(ans , value[A[i]+1] + i)
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n],B[n];
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            A[i]--;
+        }
+        
+        for(int i=0;i<n;++i){
+            cin>>B[i];
+            B[i]--;
+        }
+        
+        int ans=n-1;
+        int sz = 2*n;
+
+        vector<int>used(sz);
+       
+        
+        int last = 0;
+        for(int i=0;i<n;++i){
+            if(i==0){
+               for(int j = last;j<=B[i];j++){
+                   used[j]=i;
+               }
+               last = B[i]+1;
+            }
+            else{
+                if(last<=B[i]){
+                    for(int j = last;j<=B[i];j++){
+                        used[j] = i;
+                    }
+                    last = B[i]+1;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;++i){
+            ans = min(ans,used[A[i]+1]+i);
+        }
+        
+        cout<<ans<<"\n";
+    }
+}
