120a70f68b634482bc4d43f0cef9d717b79d7037
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-19 23:42:33 +0530

    Update Find value of variable after performing operations.cpp

--- Day94/Find value of variable after performing operations.cpp
+++ Day94/Find value of variable after performing operations.cpp
@@ -3,3 +3,17 @@ Platform:- Leetcode
 Contest :- Leetcode Weekly contest 259
 Approach :- find X++,++X and add '1' else add -1
 */
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        map<string,int>P;
+        P["X++"]=1;
+        P["++X"]=1;
+        P["--X"]=-1;
+        P["X--"]=-1;
+        int ans=0;
+        for(auto &x:operations)ans+=P[x];
+        
+        return ans;
+    }
+};
