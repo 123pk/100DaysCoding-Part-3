bbc495ec8270b2b2feee3e44535e4978221ce80a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-20 21:37:53 +0530

    Update FAKESWAP.cpp

--- Day95/FAKESWAP.cpp
+++ Day95/FAKESWAP.cpp
@@ -3,3 +3,29 @@ Platform :- Codechef
 Contest :- Codechef COOK OFF September 2021
 Approach :- we cannot convert 'p' to string 's' if p contains only one type of character either '0' or '1' else we can convert 's' to 'p'
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        string s,p;
+        cin>>s>>p;
+        
+        if(s==p)cout<<"YES\n";
+        else{
+            int c=0,d=0;
+            for(auto x:p){
+                if(x=='1')c++;
+                else d++;
+            }
+            if(c&&d)cout<<"YES\n";
+            else cout<<"NO\n";
+        }
+    }
+}
