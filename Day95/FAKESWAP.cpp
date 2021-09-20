504b723117a54691463562a140d1f9cf495b9a3d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-20 21:37:37 +0530

    Create FAKESWAP.cpp

---
+++ Day95/FAKESWAP.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Codechef COOK OFF September 2021
+Approach :- we cannot convert 'p' to string 's' if p contains only one type of character either '0' or '1' else we can convert 's' to 'p'
+*/
