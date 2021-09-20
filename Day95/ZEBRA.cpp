321e64a31aec1f3ca4dd20fbdd094730bf8cc0a8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-20 22:09:17 +0530

    Update ZEBRA.cpp

--- Day95/ZEBRA.cpp
+++ Day95/ZEBRA.cpp
@@ -1,5 +1,6 @@
 /*
 Platform :- Codechef
 Contest :- Codechef Cook Off September 2021
-Approach :- 
+Approach :- create an array of index where each adjacent value contain different character, now while choosing the index greedilly choose the index which should be inserted in
+            the array as we want the max index we can have , now take in account the case when 'k' is even or odd and the size of the index array.
 */
