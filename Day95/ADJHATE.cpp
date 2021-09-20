d509343d9f1e45d7b16cef802c8019c4a46185cf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-20 22:11:34 +0530

    Create ADJHATE.cpp

---
+++ Day95/ADJHATE.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Cook off September 2021
+Approach :- if there is only odd or even element in array then we print -1 else we print all the even elments first and then odd elements or vice versa.
+*/
