809418cf64adcc529493bb48e11e0eac54373d29
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-20 21:33:46 +0530

    Create PAIRDIST.cpp

---
+++ Day95/PAIRDIST.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Codechef
+Contest :- Codechef Cook Off September 2021
+Approach :- we need to analyse the problem then we will find that there is no solution for 'n'>4 , else we print the following way
+            if( i%4 == 0 ) print 0 ,A[i]
+            if( i%4 == 0 ) print A[i],0
+            if( i%4 == 0 ) print 0,-A[i]
+            if( i%4 == 0 ) print -A[i],0
+*/
