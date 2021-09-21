a9f55b4f56c20b0efe5cd5c27192c41d8ba5ff3e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-21 22:17:14 +0530

    Create C - Slay the dragon .cpp

---
+++ Day96/C - Slay the dragon .cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational Round 114
+Approach :- There are two options for us in each case either we will choose a value where we have 'value'<x else we choose a 'value' >= 'x' , so we do binary search and will
+            find the value just greater or equal than 'x' and biggest value >'x' we can use 'binary_search' or "set.lower_bound()" in c++ to do so in "logN" time.
+*/
