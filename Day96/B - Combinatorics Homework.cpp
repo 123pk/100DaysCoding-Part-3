58550cfae95b6fa6fde5c082b343c05eff1779f6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-21 22:07:46 +0530

    Create B - Combinatorics Homework.cpp

---
+++ Day96/B - Combinatorics Homework.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Educational Round 114
+Approach :- we try to find the max and min no of equal adjacent pairs that we can form and if 'm' lies between min-max range we print "yes" else "no"
+*/
