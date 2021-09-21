35a12748ee4654e4c7d44ad77fbfcf6f281542aa
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-21 22:04:35 +0530

    Update A - Regular Bracket sequence.cpp

--- Day96/A - Regular Bracket sequence.cpp
+++ Day96/A - Regular Bracket sequence.cpp
@@ -3,3 +3,27 @@ Platform :- Codeforces
 Contest :- Educational Round 114
 Approach :- Start with ((())) and then (())() and then ()()() , we print the pattern in following pattern
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        int x = n;
+        for(int i=n;i>=1;--i){
+            for(int j=1;j<=i;++j)cout<<"(";
+            for(int j=1;j<=i;++j)cout<<")";
+            
+            int temp = x-i;
+            for(int j=1;j<=temp;++j){
+                cout<<"()";
+            }
+            cout<<"\n";
+        }
+        
+    }
+}
