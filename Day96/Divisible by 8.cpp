cb161ce80b32233f943a0d50ac2b9f02b2b7c4fa
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-21 22:35:40 +0530

    Create Divisible by 8.cpp

---
+++ Day96/Divisible by 8.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Educational Round 114
+Approach :- We know for a number to be divisible by 8 its last 3 digits should be divisible by 8 , so we try to find all the any number from 0 to 999 which is divisible by 8
+            and is subsequence of given string .
+*/
