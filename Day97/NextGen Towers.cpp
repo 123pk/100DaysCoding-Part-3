fadfd57540bf54d3d7e1e640709f4e383f54fa8b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-22 23:23:39 +0530

    Create NextGen Towers.cpp

---
+++ Day97/NextGen Towers.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Geeksforgeeks
+Approach :- Find all the disjoint set or disjoing mini graphs and store the number of nodes in vector , now try to find the max sum of elements of that array less than equal to K
+            which is standard problem and is done using dp .
+*/
