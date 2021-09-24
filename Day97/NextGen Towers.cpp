05283be80d7d47c1290b5ab7c24e9c846da48b84
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-24 23:31:45 +0530

    Update NextGen Towers.cpp

--- Day97/NextGen Towers.cpp
+++ Day97/NextGen Towers.cpp
@@ -5,127 +5,103 @@ Approach :- Find all the disjoint set or disjoing mini graphs and store the numb
             which is standard problem and is done using dp .
 */
 //Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
 
  // } Driver Code Ends
 //User function Template for C++
- //filling my segment tree
-void make_Seg_tree(vector<pair<int,pair<int,int>>>& A,int idx){
+int printDistSum(vector<int> arr, int n,int K)
+{
+    int sum = 0;
+    for (int i=0; i<n; i++)
+        sum += arr[i];
+ 
+    // dp[i][j] would be true if arr[0..i-1] has
+    // a subset with sum equal to j.
+    bool dp[n+1][sum+1];
+    memset(dp, 0, sizeof(dp));
+ 
+    // There is always a subset with 0 sum
+    for (int i=0; i<=n; i++)
+        dp[i][0] = true;
+ 
+    // Fill dp[][] in bottom up manner
+    for (int i=1; i<=n; i++)
+    {
+        dp[i][arr[i-1]] = true;
+        for (int j=1; j<=sum; j++)
+        {
+            // Sums that were achievable
+            // without current array element
+            if (dp[i-1][j] == true)
+            {
+                dp[i][j] = true;
+                dp[i][j + arr[i-1]] = true;
+            }
+        }
+    }
     
-    for(int i=idx;i>=0;--i){
-        A[i].first = max(A[2*i+1].first , A[2*i+2].first);
-        A[i].second = {(A[2*i+1].second.first),(A[2*i+2].second.second)};
+    int ans=0;
+    // Print last row elements
+    for (int j=0; j<=sum; j++){
+        if (dp[n][j]==true){
+            if(j<=K){
+              ans=j;
+            }
+            else break;
+        }
+        
+        if(j>K)break;
     }
-}
-
-//answering queries of my segment tree
-int query(vector< pair< int, pair<int,int>> >& A ,int lo_lim ,int up_lim,int idx){
-   
-    //in case range of node and target range are disjoint
-    if( A[idx].second.first > up_lim || A[idx].second.second < lo_lim){
-        return 0;
-    }
+            
     
-    // if completely overlap
-    if(A[idx].second.first>= lo_lim && A[idx].second.second <= up_lim){
-        return A[idx].first;
-    }
-    
-    //else we have partial overlapping range
-    int val = max(query(A,lo_lim,up_lim,2*idx+1) , query(A,lo_lim,up_lim,2*idx+2));
-    return val;
-}
-
-
-//doing updates in  my segment tree
-void update(vector<pair<int,pair<int,int>>>& A, int idx ,int val){
-    A[idx].first = val;
-    idx=(idx-1)/2;
-    while(1){
-     //   cout<<idx<<" "<<(idx*2+1)<<" "<<(idx*2)<<endl;
-        A[idx].first = max(A[idx*2 + 1].first , A[idx*2+2].first);
-        if(idx==0)break;
-        idx=(idx-1)/2;
-    }
+    return ans;
 }
 
 class Solution {
-  public:
-    int solve(int n, int K, vector<int> height) {
-         
-        int ans=0;
+public:
+    int maxTowers(int n, int m, vector<vector<int>> connections, int X){
+        // code here
+        vector<int>graph[n];
+        for(int i=0;i<m;++i){
+            graph[connections[i][0]].push_back(connections[i][1]);
+            graph[connections[i][1]].push_back(connections[i][0]);
+        }
         
-        int d=0;
-     string temp=bitset<32>(n).to_string();
-     reverse(temp.begin(),temp.end());
-     int c=0;
-     
-     for(int i=0;i<32;++i){
-         if(temp[i]=='1'){
-             c++;
-             d=i;
-         }
-     }
-     
-     if(c>1)d++;
-     d=pow(2,d);
-     int x=d-n;
-     d*=2;
-     d-=1;
-     
-     //this is our seg tree  leaf nodes
-     vector<pair<int,pair<int,int>>> seg_tree(d);
-     int i=0;
-     for(int j=(d-x)-n;j<(d-x);++j){
-          seg_tree[j].first=height[i];
-          i++;
-     }
-     
-     //initialising range of each node
-     for(int i=(d-x)-n;i<d;++i){
-         seg_tree[i].second={i,i};
-     }
-     
-     // making my segment tree
-     make_Seg_tree(seg_tree,(d-x)-n-1);
-     //for(auto x:seg_tree)cout<<x.first<<" "<<x.second.first<<" "<<x.second.second<<endl;
-     int start = (d-x)-n;
-     for(int i=0;i<n;++i){
-         if((i+K)<n-1){
-             int temp = query(seg_tree,i+1+start,i+K+start,0);
-             if(height[i]>temp)ans++;
-         }
-         else{
-             int temp = query(seg_tree,i+1+start,d-1,0);
-             if(height[i]>temp)ans++;
-         }
-     }
+        int c=0,ans=0;
+        vector<int>used(n);
+        vector<int>choose;
+        for(int i=0;i<n;++i){
+            if(used[i]==0){
+                c=0;
+                used[i]=1;
+                queue<int>bfs;
+                bfs.push(i);
+                c=1;
+                while(!bfs.empty()){
+                    int sz = bfs.size();
+                    for(int j=0;j<sz;++j){
+                        int temp = bfs.front();
+                        bfs.pop();
+                        
+                        for(int k=0;k<graph[temp].size();++k){
+                            if(used[graph[temp][k]]==0){
+                                used[graph[temp][k]]=1;
+                                c++;
+                                bfs.push(graph[temp][k]);
+                            }
+                        }
+                    }
+                }
+              //  cout<<c<<" ";
+                if(c<=X)choose.push_back(c);
+            }
+        }
         
-        ans=max(ans,1);
+        sort(choose.begin(),choose.end());
+       int m1=choose.size();
+        ans = printDistSum(choose,m1,X);
         return ans;
     }
 };
-
-
-// { Driver Code Starts.
-
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int N, K;
-        cin >> N >> K;
-        vector<int> A(N);
-
-        for (int i = 0; i < N; i++) cin >> A[i];
-
-        Solution ob;
-
-        cout << ob.solve(N, K, A) << "\n";
-    }
-    return 0;
-}
-  // } Driver Code Ends
