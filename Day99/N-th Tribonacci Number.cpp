49bbaeecc277aa57838d5296465d1e6c27f14fdf
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-24 20:18:27 +0530

    Update N-th Tribonacci Number.cpp

--- Day99/N-th Tribonacci Number.cpp
+++ Day99/N-th Tribonacci Number.cpp
@@ -2,3 +2,18 @@
 Platform :- Leetcode
 Approach :- Bruteforce the formulae given
 */
+class Solution {
+public:
+    int tribonacci(int n) {
+        int A[n+1];
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(n==2)return 1;
+        A[0]=0;A[1]=1;A[2]=1;
+        for(int i=3;i<=n;++i)
+        {
+            A[i]=A[i-1]+A[i-2]+A[i-3];
+        }
+        return A[n];
+    }
+};
