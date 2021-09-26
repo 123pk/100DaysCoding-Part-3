26b520e3c7360122051c4fbb53b0cb63afea861d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-26 21:03:29 +0530

    Update UNQEQ.cpp

--- Day100/UNQEQ.cpp
+++ Day100/UNQEQ.cpp
@@ -1,4 +1,6 @@
 /*
 Platform:- Codechef
-Contest :- Codechef September Lunch time
+Contest :- Codechef September Lunch time 2021
+Approach :- Find a value n*(n+1)/2 which is basically the sum of first 'n' values from 1 to 'n' , if this value is odd then we cannot divide into equal sum values , else
+            we take one biggest value and one smallest value in put in one array upto n/2 terms then we push the remaining one in the array 2
 */
