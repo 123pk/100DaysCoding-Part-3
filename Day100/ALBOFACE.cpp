27a95b776dd27f02276381fc47186d84716b7eca
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-26 20:54:33 +0530

    Update ALBOFACE.cpp

--- Day100/ALBOFACE.cpp
+++ Day100/ALBOFACE.cpp
@@ -1,4 +1,10 @@
 /*
 Platform:- Codechef
 Contest :- Codechef September Lunch time
+Approach :- If you will analyse carefully you will found out for power of 2 >4 , let val = power_of_2 >4 then :-
+            1. if val == n then "Alice" wins
+            2. if n == val-1 then also "Alice" wins
+            3. for val-2 and val-3, "Bob" wins
+            
+            else for odd values of n we  have "Bob" as winner and for even value we have "Alice"
 */
