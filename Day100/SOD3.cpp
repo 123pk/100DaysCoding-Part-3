ec16fb0f5a119e871630b4994c2bb6a8d6fc8f0a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-26 20:56:46 +0530

    Update SOD3.cpp

--- Day100/SOD3.cpp
+++ Day100/SOD3.cpp
@@ -1,4 +1,5 @@
 /*
 Platform:- Codechef
 Contest :- Codechef September Lunch time
+Approach :- Find number of values divisble by 3 till l-1 and no of values divisible by 3 upto r and absolute difference gives us the number of values divisible by 3 in range [l,r]
 */
