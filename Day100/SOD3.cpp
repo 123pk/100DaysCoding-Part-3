f713fd6128bdfa4bd8a68ba3a0cbb67eb63ddd0e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-09-26 20:57:23 +0530

    Update SOD3.cpp

--- Day100/SOD3.cpp
+++ Day100/SOD3.cpp
@@ -3,3 +3,22 @@ Platform:- Codechef
 Contest :- Codechef September Lunch time
 Approach :- Find number of values divisble by 3 till l-1 and no of values divisible by 3 upto r and absolute difference gives us the number of values divisible by 3 in range [l,r]
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+ 
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long l,r;
+        cin>>l>>r;
+        
+        long long ans=r/3;
+        long long z=(l-1)/3;
+        ans=(ans-z);
+         
+        cout<<ans<<"\n";
+    }
+}
